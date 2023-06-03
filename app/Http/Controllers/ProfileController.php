9784d263ba116d3e11ce6d35c44786228a6a24e4
Faiz <iamfaizahmed123@gmail.com>
2023-06-03 19:54:46 +0100

    Update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -75,38 +75,46 @@ class ProfileController extends Controller
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
         $user = $request->user();
-        
+
         $user->fill($request->validated());
-    
+
         if ($user->isDirty('email')) {
             $user->email_verified_at = null;
         }
-    
+
         $user->save();
-    
+
         $incomingData = $this->buildIncomingData($request->selected_states);
-        
+
         UserCallTypeState::updateUserCallTypeState($user, $incomingData);
-    
+
         return Redirect::route('profile.edit');
     }
-    
+
     private function buildIncomingData($selectedStates): array
     {
+        // Initialize an empty array to store the call type and state combinations.
         $incomingData = [];
-    
+
+        // For each selected state item in the provided list
         foreach ($selectedStates as $item) {
+            // Extract the call type ID
             $typeId = $item['typeId'];
+            // Extract the list of selected state IDs
             $stateIds = $item['selectedStateIds'];
-    
+
+            // For each state ID in the list of selected state IDs
             foreach ($stateIds as $stateId) {
+                // Add a new entry to the incoming data with the current call type ID and state ID.
+                // Each entry represents a combination of a call type and state that the user has selected.
                 $incomingData[] = [
                     'call_type_id' => $typeId,
                     'state_id' => $stateId,
                 ];
             }
         }
-    
+
+        // Return the populated incoming data array.
         return $incomingData;
     }
 
