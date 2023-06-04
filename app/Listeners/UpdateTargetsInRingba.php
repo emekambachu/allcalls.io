b6d65682d442d715ce6d41b4b3edb025ccc4ef8e
Faiz <iamfaizahmed123@gmail.com>
2023-06-04 03:58:17 +0100

    Update

--- app/Listeners/UpdateTargetsInRingba.php
+++ app/Listeners/UpdateTargetsInRingba.php
@@ -3,6 +3,7 @@
 namespace App\Listeners;
 
 use Exception;
+use App\Models\User;
 use App\Models\State;
 use App\Models\CallType;
 use App\Services\Ringba;
@@ -32,5 +33,34 @@ class UpdateTargetsInRingba
         Log::debug($event->userId);
         Log::debug($event->toDelete);
         Log::debug($event->toInsert);
+
+
+        $ringba = new Ringba;
+
+        $user = User::find($event->userId);
+        $targets = $ringba->getAllTargets();
+
+        foreach ($event->toDelete as $record) {
+            $callTypeId = $record['call_type_id'];
+            $callType = CallType::find($callTypeId);
+
+            $targetName = $user->first_name . ' ' . $user->last_name . ' - ' . $user->id . ' - ' . $callType->type . ' - ' . $callType->id;
+
+            foreach ($targets as $target) {
+                if ($target['name'] == $targetName) {
+                    $ringba->deleteTarget($target['id']);
+                    break;
+                }
+            }
+        }
+
+        foreach ($event->toInsert as $record) {
+            $callTypeId = $record['call_type_id'];
+            $callType = CallType::find($callTypeId);
+            $state = State::find($record['state_id']);
+
+            $targetName = $user->first_name . ' ' . $user->last_name . ' - ' . $user->id . ' - ' . $callType->type . ' - ' . $callType->id;
+            $ringba->createTarget($targetName, '+18045172235', [$state->name]);
+        }
     }
 }
