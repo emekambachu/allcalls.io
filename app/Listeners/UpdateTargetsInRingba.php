5cda9b0387d7567def3826363a6bfe83764aa12d
Faiz <iamfaizahmed123@gmail.com>
2023-06-04 04:22:10 +0100

    Update

--- app/Listeners/UpdateTargetsInRingba.php
+++ app/Listeners/UpdateTargetsInRingba.php
@@ -8,6 +8,7 @@ use App\Models\State;
 use App\Models\CallType;
 use App\Services\Ringba;
 use App\Models\UserCallTypeState;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Auth\Events\Registered;
 use App\Events\UserCallTypeStateUpdated;
@@ -34,43 +35,45 @@ class UpdateTargetsInRingba
         Log::debug($event->toDelete);
         Log::debug($event->toInsert);
 
-
+        $user = User::find($event->userId);
         $ringba = new Ringba;
 
-        $user = User::find($event->userId);
-        $targets = $ringba->getAllTargets();
 
-        foreach ($event->toDelete as $record) {
-            $callTypeId = $record['call_type_id'];
+        $callTypeIds = array_unique(
+            array_merge(
+                array_column($event->toDelete, 'call_type_id'),
+                array_column($event->toInsert, 'call_type_id')
+            )
+        );
+
+        // Deleting Targets
+        foreach ($callTypeIds as $callTypeId) {
             $callType = CallType::find($callTypeId);
-
             $targetName = $user->first_name . ' ' . $user->last_name . ' - ' . $user->id . ' - ' . $callType->type . ' - ' . $callType->id;
 
+            $targets = $ringba->getAllTargets();
             foreach ($targets as $target) {
-                if ($target['name'] == $targetName) {
+                if ($target['name'] === $targetName) {
                     $ringba->deleteTarget($target['id']);
                     break;
                 }
             }
         }
 
-        $groupedToInsert = [];
-
-        foreach ($event->toInsert as $record) {
-            $groupedToInsert[$record['call_type_id']][] = $record;
-        }
-        
-        foreach ($groupedToInsert as $callTypeId => $records) {
+        // Inserting Targets
+        foreach ($callTypeIds as $callTypeId) {
             $callType = CallType::find($callTypeId);
-            $stateNames = [];
-        
-            foreach ($records as $record) {
-                $state = State::find($record['state_id']);
-                $stateNames[] = $state->name;
-            }
-        
+            $states = DB::table('users_call_type_state')
+                ->where('user_id', $user->id)
+                ->where('call_type_id', $callTypeId)
+                ->pluck('state_id')
+                ->map(function ($stateId) {
+                    return State::find($stateId)->name;
+                })
+                ->toArray();
+
             $targetName = $user->first_name . ' ' . $user->last_name . ' - ' . $user->id . ' - ' . $callType->type . ' - ' . $callType->id;
-            $ringba->createTarget($targetName, '+18045172235', $stateNames);
-        }        
+            $ringba->createTarget($targetName, '+18045172235', $states);
+        }
     }
 }
