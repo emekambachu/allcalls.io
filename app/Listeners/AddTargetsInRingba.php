073c0c91cc854721f0e16e4bce1d4563d86c638f
Faiz <iamfaizahmed123@gmail.com>
2023-07-15 01:03:00 +0100

    Update

--- app/Listeners/AddTargetsInRingba.php
+++ app/Listeners/AddTargetsInRingba.php
@@ -29,28 +29,20 @@ class AddTargetsInRingba
         Log::debug('Add Targets In Ringba Called');
 
         $user = $event->user;
+        Log::debug('Registered User:', ['user' => $user->toArray()]);
 
         // Fetch all call types associated with it, then fetch all states associated with them.
         $records = UserCallTypeState::forUser($user);
+        Log::debug('UserCallTypeState Records:', ['records' => $records->toArray()]);
+
         $callTypeIds = array_values(array_unique(array_column($records->toArray(), 'call_type_id')));
+        Log::debug('Call Type IDs:', ['callTypeIds' => $callTypeIds]);
+
         $selectedCallTypes = CallType::whereIn('id', $callTypeIds)->get();
+        Log::debug('Selected CallTypes:', ['selectedCallTypes' => $selectedCallTypes->toArray()]);
+
         $allStates = State::all();
-
-        Log::debug([
-            'records' => $records
-        ]);
-
-        Log::debug(json_encode([
-            'callTypeIds' => $callTypeIds
-        ]));
-
-        Log::debug(json_encode([
-            'selectedCallTypes' => $selectedCallTypes
-        ]));
-
-        Log::debug(json_encode([
-            'allStates' => $allStates
-        ]));
+        Log::debug('All States:', ['allStates' => $allStates->toArray()]);
 
         $selectedCallTypes = $selectedCallTypes->map(function($callType) use ($records, $allStates) {
             $currentCallTypeRecords = $records->filter(function($record) use ($callType) {
@@ -68,6 +60,8 @@ class AddTargetsInRingba
             return $callType;
         });
 
+        Log::debug('Mapped Selected CallTypes:', ['selectedCallTypes' => $selectedCallTypes->toArray()]);
+
         $ringba = new Ringba();
         $faker = Factory::create('en_US');
 
@@ -77,23 +71,29 @@ class AddTargetsInRingba
 
             // Get all used phone numbers.
             $usedPhoneNumbers = UsedPhoneNumber::all()->pluck('phone')->toArray();
+            Log::debug('Used Phone Numbers:', ['usedPhoneNumbers' => $usedPhoneNumbers]);
 
             // Generate a unique phone number.
             $phoneNumber = $faker->phoneNumber;
             while(in_array($phoneNumber, $usedPhoneNumbers)) {
                 $phoneNumber = $faker->phoneNumber;
             }
+            Log::debug('Generated Phone Number:', ['phoneNumber' => $phoneNumber]);
 
             // Store the generated number in the database.
             UsedPhoneNumber::create(['phone' => $phoneNumber]);
-        
+
             try {
                 $response = $ringba->createTarget($targetName, $phoneNumber, $targetStates);
+                Log::debug('Create Target Response:', ['response' => $response]);
+
                 $targetId = $response['target']['id']; // Fetch the target ID from the response
+                Log::debug('Target ID:', ['targetId' => $targetId]);
 
                 // Fetch all call plans
                 $responseCallPlans = $ringba->getCallPlans();
                 $callPlans = $responseCallPlans['callPlans'];
+                Log::debug('Call Plans:', ['callPlans' => $callPlans]);
 
                 // Find the call plan that matches the current call type
                 $matchingCallPlan = collect($callPlans)->firstWhere('name', $type->type);
@@ -102,15 +102,16 @@ class AddTargetsInRingba
                     // Add a route to the call plan
                     try {
                         $ringba->addRouteToCallPlan($matchingCallPlan['id'], $targetId, $phoneNumber, $targetStates);
+                        Log::debug('Added Route to Call Plan:', ['callPlanId' => $matchingCallPlan['id'], 'targetId' => $targetId, 'phoneNumber' => $phoneNumber, 'targetStates' => $targetStates]);
                     } catch (Exception $e) {
-                        echo 'Error adding route to call plan: ' . $e->getMessage();
+                        Log::error('Error adding route to call plan: ' . $e->getMessage());
                     }
                 } else {
-                    echo "No matching call plan found for type: " . $type->type;
+                    Log::warning("No matching call plan found for type: " . $type->type);
                 }
 
             } catch (Exception $e) {
-                echo 'Error creating target: ' . $e->getMessage();
+                Log::error('Error creating target: ' . $e->getMessage());
             }
         }
     }
