661aea108d9fdd4e2d810a0ab43b6a117311c618
Faiz <iamfaizahmed123@gmail.com>
2023-07-23 07:55:15 +0100

    Update

--- app/Http/Controllers/IncomingCallController.php
+++ app/Http/Controllers/IncomingCallController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\CallTypeNumber;
+use App\Models\AvailableNumber;
 use Illuminate\Support\Facades\Log;
 
 class IncomingCallController extends Controller
@@ -13,6 +15,21 @@ class IncomingCallController extends Controller
         if ($request->has('To')) {
             $to = $request->input('To');
             Log::debug('To attribute: ' . $to);
+
+            // Remove the "+1" from the beginning of the "To" number
+            $to = substr($to, 2);
+
+            // Check if the number exists in the AvailableNumber model
+            $availableNumber = AvailableNumber::where('phone', $to)->first();
+            if ($availableNumber) {
+                Log::debug('Number found in AvailableNumber model: ' . $to);
+            }
+
+            // Check if the number exists in the CallTypeNumber model
+            $callTypeNumber = CallTypeNumber::where('phone', $to)->first();
+            if ($callTypeNumber) {
+                Log::debug('Number found in CallTypeNumber model: ' . $to);
+            }
         }
 
         $twiml = '<?xml version="1.0" encoding="UTF-8"?>';
