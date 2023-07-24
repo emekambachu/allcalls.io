4934deab251f6de10886a5478873af007fefad13
Faiz <iamfaizahmed123@gmail.com>
2023-07-24 02:41:40 +0100

    Update

--- app/Http/Controllers/IncomingCallController.php
+++ app/Http/Controllers/IncomingCallController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\CallTypeNumber;
 use App\Models\AvailableNumber;
+use App\Models\User;
 use Illuminate\Support\Facades\Log;
 
 class IncomingCallController extends Controller
@@ -32,8 +33,7 @@ class IncomingCallController extends Controller
             $callTypeNumber = CallTypeNumber::where('phone', $to)->first();
             if ($callTypeNumber) {
                 Log::debug('Number found in CallTypeNumber model: ' . $to);
-                // You can handle CallTypeNumber logic here or continue with your previous TWiML
-                $twiml .= '<Response><Say voice="alice" language="en-US">Hello, this is a test of the Say verb. Hope you have a great day!</Say></Response>';
+                $twiml = $this->handleCallTypeNumberCall($to);
             }
         } else {
             $twiml .= '<Response><Say voice="alice" language="en-US">Hello, this is a test of the Say verb. Hope you have a great day!</Say></Response>';
@@ -54,5 +54,49 @@ class IncomingCallController extends Controller
         $twiml = '<Response><Dial callerId="+13186978047"><Client>' . $userId . '</Client></Dial></Response>';
 
         return $twiml;
+    }
+
+    public function handleCallTypeNumberCall($to)
+    {
+        // Determine the call type based on the 'To' number
+        $callTypeNumber = CallTypeNumber::where('phone', $to)->first();
+        $callType = $callTypeNumber->call_type;
+
+        Log::debug('Call type: ' . $callType);
+
+        // Fetch all online users who have selected the same call type and state
+        $users = $this->getOnlineUsers($callType);
+
+        // Select a user from this group
+        $selectedUser = $users;
+
+        Log::debug('Selected user: ' . $selectedUser->id);
+
+        // Find one of the available numbers and associate it with the selected user
+        $availableNumber = $this->getAvailableNumberForUser($selectedUser);
+
+        // Return the available number in a <Dial> verb to forward the call to this number
+        $twiml = '<Response><Dial><Number>' . $availableNumber->phone . '</Number></Dial></Response>';
+
+        return $twiml;
+    }
+
+    public function getOnlineUsers($callType)
+    {
+        // This method should return all online users who have selected the same call type and state
+        // This is just a placeholder method. You will need to implement this logic based on your application.
+        // For now, just get the first user
+        return User::first();
+    }
+
+    public function getAvailableNumberForUser($user)
+    {
+        // This method should return one of the available numbers and associate it with the selected user
+        // This is just a placeholder method. You will need to implement this logic based on your application.
+        $availableNumber = AvailableNumber::first();
+        $availableNumber->user_id = $user->id;
+        $availableNumber->save();
+
+        return $availableNumber;
     }
 }
