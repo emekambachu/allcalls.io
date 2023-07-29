97093c711d547aea7148f4d65f55fbcefd490b5c
Faiz <iamfaizahmed123@gmail.com>
2023-07-29 06:06:57 +0100

    Update

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -2,8 +2,8 @@
 
 namespace Database\Factories;
 
-use App\Models\Client;
 use App\Models\User;
+use App\Models\Client;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ClientFactory extends Factory
@@ -12,24 +12,25 @@ class ClientFactory extends Factory
 
     public function definition()
     {
-        // Create a new user for each client
-        $user = User::factory()->create();
+        $faker = \Faker\Factory::create('en_US');
+        $callDurationInSeconds = $faker->numberBetween(240, 600);
+        $ratePerMinute = 50; // Rate of $50 per minute
 
         return [
-            'first_name' => $this->faker->firstName,
-            'last_name' => $this->faker->lastName,
-            'phone' => $this->faker->phoneNumber,
-            'zipCode' => $this->faker->postcode,
-            'email' => $this->faker->unique()->safeEmail,
-            'address' => $this->faker->address,
-            'dob' => $this->faker->date('Y-m-d', 'now'),
+            'first_name' => $faker->firstName,
+            'last_name' => $faker->lastName,
+            'phone' => $faker->phoneNumber,
+            'zipCode' => $faker->postcode,
+            'email' => $faker->unique()->safeEmail,
+            'address' => $faker->address,
+            'dob' => $faker->dateTimeBetween('-66 years', '-23 years')->format('Y-m-d'), // Generating dob for people aged between 23 to 66 years
             'call_taken' => now(),
-            'call_duration_in_seconds' => $this->faker->numberBetween(1, 3600),
-            'hung_up_by' => $this->faker->randomElement(['Caller', 'Agent']),
-            'amount_spent' => $this->faker->randomFloat(2, 0, 999999.99),
-            'call_id' => $this->faker->uuid,
-            'call_type_id' => $this->faker->numberBetween(1, 6),
-            'user_id' => $user->id,
+            'call_duration_in_seconds' => $callDurationInSeconds,
+            'hung_up_by' => $faker->randomElement(['Caller', 'Agent']),
+            'amount_spent' => ($callDurationInSeconds / 60) * $ratePerMinute, // Calculating the amount spent based on the call duration and rate per minute
+            'call_id' => $faker->uuid,
+            'call_type_id' => $faker->numberBetween(1, 6),
+            'user_id' => User::factory(), // assuming User model is related to Client model
         ];
     }
 }
