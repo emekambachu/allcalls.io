6522270554725f06d5fa37aaa1717138eaa21e6e
Faiz <iamfaizahmed123@gmail.com>
2023-07-29 22:05:19 +0100

    Update

--- resources/js/Pages/Clients/Index.vue
+++ resources/js/Pages/Clients/Index.vue
@@ -95,7 +95,7 @@ let formatMoney = (amount) => {
       <div
         class="max-w-sm p-6 bg-custom-darksky rounded-lg shadow overflow-auto"
       >
-        <p class="mb-1 text-sm text-gray-400">Total Earned</p>
+        <p class="mb-1 text-sm text-gray-300">Total Earned</p>
         <h2 class="mb-2 text-2xl sm:text-3xl lg:text-4xl font-bold text-white">
           ${{ formatMoney(totalAmountSpent) }}
         </h2>
@@ -103,7 +103,7 @@ let formatMoney = (amount) => {
       <div
         class="max-w-sm p-6 bg-custom-darksky rounded-lg shadow overflow-auto"
       >
-        <p class="mb-1 text-sm text-gray-400">Average Call Duration</p>
+        <p class="mb-1 text-sm text-gray-300">Average Call Duration</p>
         <h2 class="mb-2 text-2xl sm:text-3xl lg:text-4xl font-bold text-white">
           {{ formatTime(averageCallDuration) }}
         </h2>
