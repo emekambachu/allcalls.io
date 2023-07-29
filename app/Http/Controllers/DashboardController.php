73c4fb2482beb438f0f5dba869493a5e1f5c6da8
Faiz <iamfaizahmed123@gmail.com>
2023-07-29 22:44:14 +0100

    Update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Inertia\Inertia;
 use App\Models\Client;
 use Illuminate\Http\Request;
@@ -11,10 +12,13 @@ class DashboardController extends Controller
 {
     public function show(Request $request)
     {
+        $sevenDaysAgo = Carbon::now()->subDays(7);
+
         $spendData = Client::select(DB::raw('date(call_taken) as date'), DB::raw('sum(amount_spent) as sum'))
-        ->groupBy('date')
-        ->orderBy('date', 'ASC')
-        ->get();
+            ->where('call_taken', '>=', $sevenDaysAgo)
+            ->groupBy('date')
+            ->orderBy('date', 'ASC')
+            ->get();
 
         return Inertia::render('Dashboard', [
             'spendData' => $spendData
