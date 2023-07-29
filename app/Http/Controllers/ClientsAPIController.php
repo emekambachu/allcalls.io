0d07a2f2011ae5a388415f85487bd784d7c80c3a
Faiz <iamfaizahmed123@gmail.com>
2023-07-29 05:43:51 +0100

    Update

--- app/Http/Controllers/ClientsAPIController.php
+++ app/Http/Controllers/ClientsAPIController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
 
 class ClientsAPIController extends Controller
 {
     public function index(Request $request)
     {
+        $perPage = 10;
+
+        $clients = Client::whereUserId($request->user->id)->paginate($perPage);
+
+        return [
+            'clients' => $clients
+        ];
     }
 }
