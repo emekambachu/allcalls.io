78c344f44519f0384024d5719892ee3883876a9b
Faiz <iamfaizahmed123@gmail.com>
2023-07-30 04:37:14 +0100

    Update

--- resources/js/Pages/Transactions/Index.vue
+++ resources/js/Pages/Transactions/Index.vue
@@ -85,13 +85,13 @@ let formatDate = (date) => {
                       v-if="transaction.sign"
                       class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"
                     >
-                      Added
+                      Deposited
                     </span>
                     <span
                       v-else
                       class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800"
                     >
-                      Removed
+                      Deducted
                     </span>
                   </td>
                 </tr>
