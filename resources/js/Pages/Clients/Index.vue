7b9f0a23f1a518c6b501406c51199941de1c7997
Faiz <iamfaizahmed123@gmail.com>
2023-07-30 05:14:06 +0100

    Update

--- resources/js/Pages/Clients/Index.vue
+++ resources/js/Pages/Clients/Index.vue
@@ -254,9 +254,8 @@ let formatMoney = (amount) => {
     >
       <div
         id="defaultModal"
-        :class="{ hidden: !showModal }"
+        v-if="showModal"
         tabindex="-1"
-        aria-hidden="!showModal"
         class="flex items-center justify-center fixed inset-0 z-50 w-full h-full overflow-x-hidden overflow-y-auto max-h-full mx-4 sm:mx-0"
       >
         <div class="relative w-full max-w-4xl max-h-full mx-auto">
