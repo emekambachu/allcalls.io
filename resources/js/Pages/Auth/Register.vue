8b80e4ffc8a284167a1ef08d058f9ab99ff98d0d
Faiz <iamfaizahmed123@gmail.com>
2023-07-31 05:45:34 +0100

    Update

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -11,7 +11,10 @@ import { Head, Link, useForm } from "@inertiajs/vue3";
 import Multiselect from "@vueform/multiselect";
 import { ref, reactive, computed } from "vue";
 // import { useVeeValidate } from '@vee-validate/vue3';
-import axios from 'axios';
+import axios from "axios";
+// If you are using PurgeCSS, make sure to whitelist the carousel CSS classes
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
 
 let step = ref(0);
 let firstStepErrors = ref({});
@@ -81,15 +84,16 @@ let form = useForm({
     acc[obj.id] = [];
     return acc;
   }, {}),
+  bids: props.callTypes.map((type) => {
+    return { call_type_id: type.id, bid: 20 };
+  }),
 });
 
 let text = ref([]);
 
-let check = () => {
+let check = () => {};
 
-}
-
-// next() { 
+// next() {
 //   this.step = 1
 //   // alert ('its working though')
 // }
@@ -100,37 +104,38 @@ let submit = () => {
     //   onFinish: () => step.value = 1,
     // })
 
-    return axios.post('/register-step-one', form)
-    .then(response => {
+    return axios
+      .post("/register-step-one", form)
+      .then((response) => {
         // This block handles the success case.
         // console.log('Validation passed', response.data);
         step.value = 1;
-    })
-    .catch(error => {
+      })
+      .catch((error) => {
         // This block handles the error case.
         // Error details are in error.response.data.
 
         if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log(error.response.data);
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
 
-            if (error.response.status === 400) {
-                // Handle validation errors here.
-                console.log('Validation errors', error.response.data.errors);
-                firstStepErrors.value = error.response.data.errors;
-            } else {
-                // Handle other types of errors.
-                console.log('Other errors', error.response.data);
-            }
+          if (error.response.status === 400) {
+            // Handle validation errors here.
+            console.log("Validation errors", error.response.data.errors);
+            firstStepErrors.value = error.response.data.errors;
+          } else {
+            // Handle other types of errors.
+            console.log("Other errors", error.response.data);
+          }
         } else if (error.request) {
-            // The request was made but no response was received
-            console.log('No response received', error.request);
+          // The request was made but no response was received
+          console.log("No response received", error.request);
         } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log('Error', error.message);
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
         }
-    });
+      });
   }
   form.post(route("register"), {
     // onFinish: () => form.reset("password", "password_confirmation"),
@@ -141,49 +146,104 @@ let submit = () => {
 <template>
   <!-- <GuestLayout> -->
   <NewGuestLayout>
-
     <Head title="Register" />
 
     <template v-slot:loadingText>
-      <div class="px-10 text-center text-4xl xl:text-5xl text-custom-white font-extrabold tracking-tighter">Start Receiving Live Calls Now!</div>
-      <div class="text-md text-blue-400 font-semibold text-center px-10 mb-6">No risk, no contracts, and no long-term commitment. Cancel anytime, hassle-free.</div>
-    </template>
-
-    <template v-slot:smallStepOneLoading >
-      <div v-show="step === 0" class="flex flex-col items-center w-full">
-        <div class="w-[80%] max-w-xl bg-gray-200 rounded-full h-1.5 dark:bg-gray-700 mb-1">
-            <div class="bg-blue-600 h-1.5 rounded-full dark:bg-blue-500" style="width: 45%"></div>
-        </div>
-        <div class="text-sm text-gray-500">Create Your Account: Step 1 of 2</div>
+      <div
+        class="px-10 text-center text-4xl xl:text-5xl text-custom-white font-extrabold tracking-tighter"
+      >
+        Start Receiving Live Calls Now!
       </div>
-
-      <div v-show="step === 1" class="flex flex-col items-center w-full">
-        <div class="w-[80%] max-w-xl bg-gray-200 rounded-full h-1.5 dark:bg-gray-700 mb-1">
-            <div class="bg-blue-600 h-1.5 rounded-full dark:bg-blue-500" style="width: 95%"></div>
-        </div>
-        <div class="text-sm text-gray-500">Create Your Account: Step 2 of 2</div>
+      <div class="text-md text-blue-400 font-semibold text-center px-10 mb-6">
+        No risk, no contracts, and no long-term commitment. Cancel anytime,
+        hassle-free.
       </div>
     </template>
 
-    <template v-slot:largeStepOneLoading >
+    <template v-slot:smallStepOneLoading>
       <div v-show="step === 0" class="flex flex-col items-center w-full">
-        <div class="w-full bg-gray-300 rounded-full h-1.5  mb-1">
-            <div class="bg-blue-600 h-1.5 rounded-full dark:bg-blue-500" style="width: 45%"></div>
+        <div
+          class="w-[80%] max-w-xl bg-gray-200 rounded-full h-1.5 dark:bg-gray-700 mb-1"
+        >
+          <div
+            class="bg-blue-600 h-1.5 rounded-full dark:bg-blue-500"
+            style="width: 15%"
+          ></div>
         </div>
-        <div class="text-sm text-gray-300">Create Your Account: Step 1 of 2</div>
+        <div class="text-sm text-gray-500">
+          Create Your Account: Step 1 of 3
+        </div>
       </div>
 
       <div v-show="step === 1" class="flex flex-col items-center w-full">
-        <div class="w-full bg-gray-300 rounded-full h-1.5  mb-1">
-            <div class="bg-blue-600 h-1.5 rounded-full dark:bg-blue-500" style="width: 95%"></div>
+        <div
+          class="w-[80%] max-w-xl bg-gray-200 rounded-full h-1.5 dark:bg-gray-700 mb-1"
+        >
+          <div
+            class="bg-blue-600 h-1.5 rounded-full dark:bg-blue-500"
+            style="width: 50%"
+          ></div>
         </div>
-        <div class="text-sm text-gray-300">Create Your Account: Step 2 of 2</div>
+        <div class="text-sm text-gray-500">
+          Create Your Account: Step 2 of 3
+        </div>
+      </div>
+
+      <div v-show="step === 2" class="flex flex-col items-center w-full">
+        <div
+          class="w-[80%] max-w-xl bg-gray-200 rounded-full h-1.5 dark:bg-gray-700 mb-1"
+        >
+          <div
+            class="bg-blue-600 h-1.5 rounded-full dark:bg-blue-500"
+            style="width: 95%"
+          ></div>
+        </div>
+        <div class="text-sm text-gray-500">
+          Create Your Account: Step 3 of 3
+        </div>
+      </div>
+    </template>
+
+    <template v-slot:largeStepOneLoading>
+      <div v-show="step === 0" class="flex flex-col items-center w-full">
+        <div class="w-full bg-gray-300 rounded-full h-1.5 mb-1">
+          <div
+            class="bg-blue-600 h-1.5 rounded-full dark:bg-blue-500"
+            style="width: 15%"
+          ></div>
+        </div>
+        <div class="text-sm text-gray-300">
+          Create Your Account: Step 1 of 3
+        </div>
+      </div>
+
+      <div v-show="step === 1" class="flex flex-col items-center w-full">
+        <div class="w-full bg-gray-300 rounded-full h-1.5 mb-1">
+          <div
+            class="bg-blue-600 h-1.5 rounded-full dark:bg-blue-500"
+            style="width: 50%"
+          ></div>
+        </div>
+        <div class="text-sm text-gray-300">
+          Create Your Account: Step 2 of 3
+        </div>
+      </div>
+
+      <div v-show="step === 2" class="flex flex-col items-center w-full">
+        <div class="w-full bg-gray-300 rounded-full h-1.5 mb-1">
+          <div
+            class="bg-blue-600 h-1.5 rounded-full dark:bg-blue-500"
+            style="width: 95%"
+          ></div>
+        </div>
+        <div class="text-sm text-gray-300">
+          Create Your Account: Step 3 of 3
+        </div>
       </div>
     </template>
 
     <form @submit.prevent="submit">
       <div v-show="step === 0">
-
         <div>
           <GuestInputLabel for="first_name" value="First Name" />
 
@@ -192,13 +252,17 @@ let submit = () => {
             type="text"
             class="mt-1 block w-full"
             v-model="form.first_name"
-            minlength="2" 
-            required 
-            pattern="[A-Za-z]{1,32}" 
+            minlength="2"
+            required
+            pattern="[A-Za-z]{1,32}"
             onkeyup="this.value=this.value.replace(/[0-9]/g,'');"
           />
           <!-- <InputError class="mt-2" :message="form.errors.first_name" /> -->
-          <div v-if="firstStepErrors.first_name" class="text-red-500" v-text="firstStepErrors.first_name[0]"></div>
+          <div
+            v-if="firstStepErrors.first_name"
+            class="text-red-500"
+            v-text="firstStepErrors.first_name[0]"
+          ></div>
         </div>
 
         <div class="mt-4">
@@ -209,13 +273,17 @@ let submit = () => {
             type="text"
             class="mt-1 block w-full"
             v-model="form.last_name"
-            required 
-            pattern="[A-Za-z]{1,32}" 
+            required
+            pattern="[A-Za-z]{1,32}"
             onkeyup="this.value=this.value.replace(/[0-9]/g,'');"
           />
 
           <!-- <InputError class="mt-2" :message="form.errors.last_name" /> -->
-          <div v-if="firstStepErrors.last_name" class="text-red-500" v-text="firstStepErrors.last_name[0]"></div>
+          <div
+            v-if="firstStepErrors.last_name"
+            class="text-red-500"
+            v-text="firstStepErrors.last_name[0]"
+          ></div>
         </div>
 
         <div class="mt-4">
@@ -231,7 +299,11 @@ let submit = () => {
           />
 
           <!-- <InputError class="mt-2" :message="form.errors.email" /> -->
-          <div v-if="firstStepErrors.email" class="text-red-500" v-text="firstStepErrors.email[0]"></div>
+          <div
+            v-if="firstStepErrors.email"
+            class="text-red-500"
+            v-text="firstStepErrors.email[0]"
+          ></div>
         </div>
 
         <div class="mt-4">
@@ -242,13 +314,17 @@ let submit = () => {
             type="text"
             class="mt-1 block w-full"
             v-model="form.phone"
-            minlength="10" 
-            maxlength="10" 
-            onkeyup="this.value=this.value.replace(/[^\d]/,&#39;&#39;)" 
+            minlength="10"
+            maxlength="10"
+            onkeyup="this.value=this.value.replace(/[^\d]/,&#39;&#39;)"
           />
 
           <!-- <InputError class="mt-2" :message="form.errors.phone" /> -->
-          <div v-if="firstStepErrors.phone" class="text-red-500" v-text="firstStepErrors.phone[0]"></div>
+          <div
+            v-if="firstStepErrors.phone"
+            class="text-red-500"
+            v-text="firstStepErrors.phone[0]"
+          ></div>
         </div>
 
         <div class="mt-4">
@@ -263,11 +339,18 @@ let submit = () => {
           />
 
           <!-- <InputError class="mt-2" :message="form.errors.password" /> -->
-          <div v-if="firstStepErrors.password" class="text-red-500" v-text="firstStepErrors.password[0]"></div>
+          <div
+            v-if="firstStepErrors.password"
+            class="text-red-500"
+            v-text="firstStepErrors.password[0]"
+          ></div>
         </div>
 
         <div class="mt-4">
-          <GuestInputLabel for="password_confirmation" value="Confirm Password" />
+          <GuestInputLabel
+            for="password_confirmation"
+            value="Confirm Password"
+          />
 
           <GuestTextInput
             id="password_confirmation"
@@ -290,7 +373,61 @@ let submit = () => {
         </div>
       </div>
 
-      <div v-show="step === 1">
+      <div v-if="step === 1">
+
+        <Carousel :items-to-show="1">
+          <Slide :key="1">
+            <div class="p-12">
+              <h6 class="text-black text-xl text-left font-bold">
+                How It Works
+              </h6>
+              <p class="text-gray-700 text-left">
+                Our dynamic bidding system allows you to set a maximum bid for
+                each type of call you're interested in as you configure your
+                account.
+              </p>
+            </div>
+          </Slide>
+          <Slide :key="2">
+            <div class="p-12">
+              <h6 class="text-black text-xl text-left font-bold">
+                The Bidding Process
+              </h6>
+              <p class="text-gray-700 text-left">
+                Each call type has a base bid of $20, but you can bid higher to
+                increase your chances of securing the call. The user with the
+                highest bid wins the call but pays only $1 more than the second
+                highest bid.
+              </p>
+            </div>
+          </Slide>
+          <Slide :key="3">
+            <div class="p-12">
+              <h6 class="text-black text-xl text-left font-bold">
+                Getting Started
+              </h6>
+              <p class="text-gray-700 text-left">
+                To start, select your desired call types and indicate your
+                maximum bid for each. Don't forget to select the states where
+                you're licensed to operate. Remember, banks will see your bid
+                amounts as they set the prices they allow for payment
+                processing, so bid wisely!
+                <button @click="step = 2">Next</button>
+              </p>
+            </div>
+          </Slide>
+
+          <template #addons>
+            <navigation />
+            <pagination />
+          </template>
+        </Carousel>
+
+      </div>
+
+      <div v-if="step === 2">
+
+
         <div class="mt-4">
           <GuestInputLabel
             class="mb-3"
@@ -298,7 +435,11 @@ let submit = () => {
             value="What types of calls do you want to receive?"
           />
 
-          <div v-for="callType in callTypes" :key="callType.id" class="mb-4">
+          <div
+            v-for="(callType, index) in callTypes"
+            :key="callType.id"
+            class="mb-4"
+          >
             <input
               :id="`insurance_type_${callType.id}`"
               type="checkbox"
@@ -306,18 +447,31 @@ let submit = () => {
               @change="onTypeUpdate"
               :value="callType.id"
             />
+
             <label
               class="ml-2 text-md font-medium text-custom-indigo"
               :for="`insurance_type_${callType.id}`"
               v-text="callType.type"
             ></label>
 
-            <div
-              v-if="selectedTypes.includes(callType.id)"
-              class="py-2 "
-            >
-              <label
-                class="ml-2 text-xs font-medium"
+            <div v-if="selectedTypes.includes(callType.id)" class="pt-2 mb-8">
+              <div>
+                <label class="ml-2 text-xs font-medium">Your Bid</label>
+                <div class="relative mb-2">
+                  <div
+                    class="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none text-white"
+                  >
+                    $
+                  </div>
+                  <input
+                    type="number"
+                    id="input-group-1"
+                    class="text-white bg-custom-blue border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
+                    placeholder="20.00"
+                  />
+                </div>
+              </div>
+              <label class="ml-2 text-xs font-medium"
                 >States you're licensed in:</label
               >
               <Multiselect
@@ -327,6 +481,7 @@ let submit = () => {
                 label="label"
                 mode="tags"
                 :close-on-select="false"
+                placeholder="Select a state"
               >
               </Multiselect>
             </div>
@@ -386,32 +541,50 @@ let submit = () => {
           </PrimaryButton>
         </div>
       </div>
+      
     </form>
 
-
-
-    
     <template v-slot:titles>
-      <div class="text-4xl lg:text-5xl xl:text-8xl text-white mb-10 ">Getting Started is Easy.<span class="text-custom-green">Create your account today!</span></div>
-    </template>
-    
-    <template v-slot:subtitles>
-      <div class="text-custom-blue font-bold text-2xl lg:text-2xl xl:text-5xl text-3xl">Try our Calls for Yourself!</div>
-      
-      <div class="text-custom-blue text-sm md:text-lg lg:text-2xl mt-6 font-bold">
-        We pride ourselves on ease of use. Once you have created an account, you will be able to select what kind of calls you would like to receive and begin speaking with customers right away! Create an account to get started.
+      <div class="text-4xl lg:text-5xl xl:text-8xl text-white mb-10">
+        Getting Started is Easy.<span class="text-custom-green"
+          >Create your account today!</span
+        >
       </div>
     </template>
-    
+
+    <template v-slot:subtitles>
+      <div
+        class="text-custom-blue font-bold text-2xl lg:text-2xl xl:text-5xl text-3xl"
+      >
+        Try our Calls for Yourself!
+      </div>
+
+      <div
+        class="text-custom-blue text-sm md:text-lg lg:text-2xl mt-6 font-bold"
+      >
+        We pride ourselves on ease of use. Once you have created an account, you
+        will be able to select what kind of calls you would like to receive and
+        begin speaking with customers right away! Create an account to get
+        started.
+      </div>
+    </template>
   </NewGuestLayout>
   <!-- </GuestLayout> -->
 
   <Footer />
-
 </template>
 
 <style src="@vueform/multiselect/themes/default.css"></style>
 <style>
+input[type="number"]::-webkit-outer-spin-button,
+input[type="number"]::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+input[type="number"] {
+  -moz-appearance: textfield;
+}
+
 .multiselect {
   color: black !important;
   border: none;
@@ -419,7 +592,29 @@ let submit = () => {
 }
 
 .multiselect-wrapper {
-  background-color: rgb(17 24 39 / var(--tw-bg-opacity));
+  background-color: #03243d;
   border-radius: 5px;
 }
+/* 
+.carousel__item {
+  min-height: 200px;
+  width: 100%;
+  background-color: var(--vc-clr-primary);
+  color: var(--vc-clr-white);
+  font-size: 20px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.carousel__slide {
+  padding: 10px;
+}
+
+.carousel__prev,
+.carousel__next {
+  box-sizing: content-box;
+  border: 5px solid white;
+} */
 </style>
