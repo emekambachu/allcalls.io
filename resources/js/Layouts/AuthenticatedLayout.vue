2d181c6cf699471fd14e8d324c9ade182bbc5d37
Faiz <iamfaizahmed123@gmail.com>
2023-07-31 23:16:36 +0100

    Update

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -22,14 +22,9 @@ let formatMoney = (amount) => {
       <div
         class="bg-custom-sky text-gray-50 py-2 px-16 flex justify-center items-center"
       >
-        <div>Download the mobile app and start listening to calls now!</div>
+        <div>Download the mobile app and start buying calls now!</div>
         <div class="ml-6">
-          <button
-            type="button"
-            class="border border-gray-400 ease-in cursor-pointer bg-white bg-opacity-5 hover:shadow-2xl hover:bg-white hover:text-custom-blue hover:bg-opacity-80 rounded px-3 py-3 font-bold text-md text-custom-white absolute right-0"
-          >
-            Download App
-          </button>
+          <button type="button" class="px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">Download App</button>
         </div>
       </div>
       <nav
