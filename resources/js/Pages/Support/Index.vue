1bc60ce2e572c9b6ba454f6285ea664fcb5a4d35
Faiz <iamfaizahmed123@gmail.com>
2023-08-03 19:08:02 +0100

    Update

---
+++ resources/js/Pages/Support/Index.vue
@@ -0,0 +1,33 @@
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head } from "@inertiajs/vue3";
+
+const props = defineProps({
+
+});
+
+</script>
+
+<template>
+  <Head title="Support" />
+  <AuthenticatedLayout>
+    <template #header>
+      <h2
+        class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight"
+      >
+        Support
+      </h2>
+    </template>
+
+    <div class="pt-14">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <div class="px-4 sm:px-8 sm:rounded-lg">
+          <div class="text-4xl text-custom-sky font-bold mb-6">Support</div>
+          <hr class="mb-4" />
+        </div>
+      </div>
+    </div>
+
+
+  </AuthenticatedLayout>
+</template>
