d791fc3bff917c4d078ba5e46fc367de5dd1fd14
Faiz <iamfaizahmed123@gmail.com>
2023-08-07 00:10:26 +0100

    Update

--- resources/js/Components/ClientDetailsModal.vue
+++ resources/js/Components/ClientDetailsModal.vue
@@ -1,13 +1,16 @@
 <script setup>
-import { defineEmits } from "vue";
+import { ref, defineEmits } from "vue";
+import TextInput from "@/Components/TextInput.vue";
 
 const { showModal, selectedClient } = defineProps([
   "showModal",
   "selectedClient",
 ]);
+const editScreen = ref(false);
 const emit = defineEmits(["close"]);
 
 const close = () => {
+  editScreen.value = false;
   emit("close");
 };
 </script>
@@ -31,7 +34,10 @@ const close = () => {
       <div class="fixed inset-0 bg-black opacity-60"></div>
       <!-- This is the overlay -->
 
-      <div class="relative w-full max-w-4xl max-h-full mx-auto">
+      <div
+        v-if="!editScreen"
+        class="relative w-full max-w-4xl max-h-full mx-auto"
+      >
         <div
           class="relative bg-sky-950 border border-custom-darksky rounded-lg shadow-lg"
         >
@@ -67,9 +73,19 @@ const close = () => {
           </div>
           <div class="p-6">
             <div v-if="selectedClient">
-              <h4 class="text-2xl font-semibold text-custom-sky mb-2">
-                Personal Details
-              </h4>
+              <div class="flex justify-between items-center">
+                <h4 class="text-2xl font-semibold text-custom-sky mb-2">
+                  Personal Details
+                </h4>
+
+                <button
+                  @click="editScreen = true"
+                  class="border border-gray-400 ease-in cursor-pointer bg-gray-400 bg-opacity-5 hover:shadow-2xl hover:bg-white hover:text-custom-blue hover:bg-opacity-80 rounded px-3 py-3 font-bold text-md text-gray-500 transition"
+                >
+                  Edit Client
+                </button>
+              </div>
+
               <div
                 class="grid sm:grid-cols-1 md:grid-cols-2 gap-2 text-gray-500 dark:text-gray-300 mb-10"
               >
@@ -175,6 +191,160 @@ const close = () => {
               </button>
             </div>
           </div>
+        </div>
+      </div>
+
+      <div v-else class="relative w-full max-w-4xl max-h-full mx-auto">
+        <div
+          class="relative bg-sky-950 border border-custom-darksky rounded-lg shadow-lg"
+        >
+          <div
+            class="flex items-start justify-between p-4 border-b rounded-t border-gray-600"
+          >
+            <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+              Edit Client Details
+            </h3>
+            <button
+              @click="close"
+              type="button"
+              class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-700 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              data-modal-hide="defaultModal"
+            >
+              <svg
+                class="w-3 h-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 14"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                />
+              </svg>
+              <span class="sr-only">Close modal</span>
+            </button>
+          </div>
+          <form class="p-6">
+            <h4 class="text-2xl font-semibold text-custom-sky mb-2">
+              Personal Details
+            </h4>
+            <div
+              class="grid sm:grid-cols-1 md:grid-cols-2 gap-2 text-gray-500 dark:text-gray-300 mb-10"
+            >
+              <div>
+                <label class="text-lg">First Name:</label>
+                <TextInput
+                  type="text"
+                  name="first_name"
+                  id="first_name"
+                  class="w-full"
+                  placeholder="John"
+                  required
+                  v-model="selectedClient.first_name"
+                />
+              </div>
+              <div>
+                <label class="text-lg">Last Name:</label>
+                <TextInput
+                  type="text"
+                  name="last_name"
+                  id="last_name"
+                  class="w-full"
+                  placeholder="Doe"
+                  required
+                  v-model="selectedClient.last_name"
+                />
+              </div>
+              <div>
+                <label class="text-lg">Date of Birth:</label>
+                <TextInput
+                  type="text"
+                  name="dob"
+                  id="dob"
+                  class="w-full"
+                  required
+                  v-model="selectedClient.dob"
+                />
+              </div>
+            </div>
+
+            <h4 class="text-2xl font-semibold text-custom-sky mb-2">
+              Contact Information
+            </h4>
+            <div
+              class="grid sm:grid-cols-1 md:grid-cols-2 gap-2 text-gray-500 dark:text-gray-300 mb-10"
+            >
+              <div>
+                <label class="text-lg">Phone:</label>
+                <TextInput
+                  type="text"
+                  name="phone"
+                  id="phone"
+                  class="w-full"
+                  required
+                  v-model="selectedClient.phone"
+                />
+              </div>
+              <div>
+                <label class="text-lg">Email:</label>
+                <TextInput
+                  type="email"
+                  name="email"
+                  id="email"
+                  class="w-full"
+                  placeholder="john@example.com"
+                  required
+                  v-model="selectedClient.email"
+                />
+              </div>
+              <div>
+                <label class="text-lg">Address:</label>
+                <TextInput
+                  type="text"
+                  name="address"
+                  id="address"
+                  class="w-full"
+                  placeholder="10001"
+                  required
+                  v-model="selectedClient.address"
+                />
+              </div>
+              <div>
+                <label class="text-lg">Zip Code:</label>
+                <TextInput
+                  type="text"
+                  name="zip"
+                  id="zip"
+                  class="w-full"
+                  placeholder="10001"
+                  required
+                  v-model="selectedClient.zipCode"
+                />
+              </div>
+            </div>
+
+            <!-- (The rest of your sections would follow a similar structure) -->
+
+            <div class="flex justify-end mt-6">
+              <button
+                type="submit"
+                class="border border-gray-400 ease-in cursor-pointer bg-white bg-opacity-5 hover:shadow-2xl hover:bg-white hover:text-custom-blue hover:bg-opacity-80 rounded px-3 py-3 font-bold text-md text-custom-white"
+              >
+                Save Changes
+              </button>
+              <button
+                @click.prevent="close"
+                type="button"
+                class="ml-4 border border-gray-400 ease-in cursor-pointer bg-white bg-opacity-5 hover:shadow-2xl hover:bg-white hover:text-custom-blue hover:bg-opacity-80 rounded px-3 py-3 font-bold text-md text-custom-white"
+              >
+                Close
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
