3f7c91e72dcf8defe234753d635a2532338db460
Faiz <iamfaizahmed123@gmail.com>
2023-08-10 09:04:37 +0100

    Update

--- resources/js/Components/DashboardFooter.vue
+++ resources/js/Components/DashboardFooter.vue
@@ -5,15 +5,15 @@ import ApplicationLogo from "@/Components/ApplicationLogo.vue";
 <template>
   <div class="border-t border-gray-800 text-white w-full mt-10 py-6">
     <div>
-      <div class="mb-2">
+      <div class="mb-1">
         <h2 class="text-lg text-custom-green text-center">Important Links</h2>
       </div>
-      <div class="flex justify-center items-center mb-4">
+      <div class="flex justify-center items-center mb-6">
         <a class="mr-2 text-xs hover:text-gray-300" href="/terms.php">Terms of Service</a>
         <span class="mr-2">|</span>
         <a class="text-xs hover:text-gray-300" href="/privacy.php"> Privacy Policy</a>
       </div>
-      <div class="mb-2">
+      <div class="mb-1">
         <h2 class="text-lg text-custom-green text-center">Contact Information</h2>
       </div>
       <div class="flex flex-col items-center text-xs">
