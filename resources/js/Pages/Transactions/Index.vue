dc5b9387e1674708baabd2162390d03fc8e311fa
awais3405 <awaisali3405@gmail.com>
2023-08-16 15:42:33 +0500

    transactions table paginate commit

--- resources/js/Pages/Transactions/Index.vue
+++ resources/js/Pages/Transactions/Index.vue
@@ -2,12 +2,13 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head, router } from "@inertiajs/vue3";
 
-defineProps({
+let props = defineProps({
   transactions: {
     type: Object,
   },
 });
 
+console.log(props.transactions);
 let formatDate = (date) => {
   if (!date) {
     return "";
@@ -31,31 +32,39 @@ let formatMoney = (amount) => {
     .toFixed(2)
     .replace(/\d(?=(\d{3})+\.)/g, "$&,");
 };
+let fetchTransactions = (page) => {
+  // Create URL object from page
+  let url = new URL(page);
+
+  // Ensure the protocol is https
+  if (url.protocol !== "https:") {
+    url.protocol = "https:";
+  }
+
+  // Get the https URL as a string
+  let httpsPage = url.toString();
+
+  router.visit(httpsPage, { method: "get" });
+};
 </script>
 
 <template>
   <Head title="Transactions" />
   <AuthenticatedLayout>
     <template #header>
-      <h2
-        class="font-semibold text-xl text-gray-800 leading-tight"
-      >
-        Transactions
-      </h2>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">Transactions</h2>
     </template>
 
     <div class="pt-14">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
         <div class="px-4 sm:px-8 sm:rounded-lg">
-          <div class="text-4xl text-custom-sky font-bold mb-6">
-            Transactions
-          </div>
+          <div class="text-4xl text-custom-sky font-bold mb-6">Transactions</div>
           <hr class="mb-4" />
         </div>
       </div>
     </div>
 
-    <section v-if="transactions.length" class="p-3">
+    <section v-if="transactions.data.length" class="p-3">
       <div class="mx-auto max-w-screen-xl sm:px-12">
         <div class="relative sm:rounded-lg overflow-hidden">
           <div class="overflow-x-auto">
@@ -70,7 +79,7 @@ let formatMoney = (amount) => {
               </thead>
               <tbody>
                 <tr
-                  v-for="transaction in transactions"
+                  v-for="transaction in transactions.data"
                   :key="transaction.id"
                   class="border-b border-gray-500"
                 >
@@ -103,6 +112,77 @@ let formatMoney = (amount) => {
                 </tr>
               </tbody>
             </table>
+            <div class="p-4">
+              <nav
+                class="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4"
+                aria-label="Table navigation"
+              >
+                <span class="text-sm font-normal text-gray-500 dark:text-gray-400">
+                  Showing
+                  <span class="font-semibold text-custom-blue">{{
+                    transactions.current_page
+                  }}</span>
+                  of
+                  <span class="font-semibold text-custom-blue">{{
+                    transactions.last_page
+                  }}</span>
+                </span>
+                <ul class="inline-flex items-stretch -space-x-px cursor-pointer">
+                  <li>
+                    <a
+                      v-if="transactions.prev_page_url"
+                      @click="fetchTransactions(transactions.prev_page_url)"
+                      class="flex items-center justify-center h-full py-1.5 px-3 ml-0 text-gray-500 bg-custom-white rounded-l-lg hover:bg-sky-950 hover:shadow-2xl hover:text-white"
+                    >
+                      <span class="sr-only">Previous</span>
+                      <svg
+                        class="w-5 h-5"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewbox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      class="flex items-center justify-center text-sm py-2 px-3 leading-tight font-extrabold text-gray-500 bg-custom-white shadow-2xl hover:bg-sky-950 hover:shadow-2xl hover:text-white"
+                      >{{ transactions.current_page }}
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      v-if="transactions.next_page_url"
+                      @click="fetchTransactions(transactions.next_page_url)"
+                      class="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-custom-white rounded-r-lg hover:bg-sky-950 hover:shadow-2xl hover:text-white"
+                    >
+                      <span class="sr-only">Next</span>
+                      <svg
+                        class="w-5 h-5"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewbox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
           </div>
         </div>
       </div>
