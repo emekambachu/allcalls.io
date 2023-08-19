4d5a661f761b54aa9d2f718d36d754fdde537359
Faiz <iamfaizahmed123@gmail.com>
2023-08-19 23:18:10 +0100

    Update

--- app/Http/Controllers/CallStatusController.php
+++ app/Http/Controllers/CallStatusController.php
@@ -3,31 +3,89 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\CallType;
 use Illuminate\Http\Request;
+use App\Events\MissedCallEvent;
+use App\Events\CompletedCallEvent;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Http;
 
 class CallStatusController extends Controller
 {
+
     public function update(Request $request)
     {
+        // 1. Validate request
         $request->validate([
-            'user_id' => 'required'
+            'user_id' => 'required',
+            'call_type_id' => 'required',
+            'DialCallStatus' => 'required',
         ]);
 
-        Log::debug('Ringing event for user ' . $request->user_id);
+        $callStatus = $request->input('DialCallStatus');
+        $userId = $request->input('user_id');
+        $callTypeId = $request->input('call_type_id');
 
-        $user = User::findOrFail($request->user_id);
+        $user = User::findOrFail($userId);
 
-        if ($user->device_token) {
-            $response = Http::post(route('call.pushNotification'), [
-                'deviceToken' => $user->device_token,
-            ]);
-            Log::debug('Notification attempt from status callback:' . $response->body());
-            return;
+        switch ($callStatus) {
+            case 'ringing':
+                Log::debug('Ringing event for user ' . $userId);
+
+                if ($user->device_token) {
+                    $response = Http::post(route('call.pushNotification'), [
+                        'deviceToken' => $user->device_token,
+                    ]);
+                    Log::debug('Notification attempt from status callback:' . $response->body());
+                    return;
+                }
+
+                Log::debug('Device token was not found for user_id ' . $user->id);
+                break;
+
+            case 'no-answer':
+                Log::debug('no-answer event for user ' . $request->user_id);
+                // Dispatch MissedCallEvent
+                MissedCallEvent::dispatch($user);
+                break;
+
+            case 'completed':
+                Log::debug('completed event for user ' . $request->user_id);
+                $callDuration = $request->input('DialCallDuration');
+
+                if ($callDuration > 60) {
+                    // Dispatch CompletedCallEvent
+                    CompletedCallEvent::dispatch($user, CallType::find($callTypeId), $callDuration);
+                }
+                break;
+
+            default:
+                Log::debug('Unhandled call status: ' . $callStatus);
+                break;
         }
 
-        Log::debug('Device token was not found for user_id ' . $user->id);
         return;
     }
+
+    // public function update(Request $request)
+    // {
+    //     $request->validate([
+    //         'user_id' => 'required'
+    //     ]);
+
+    //     Log::debug('Ringing event for user ' . $request->user_id);
+
+    //     $user = User::findOrFail($request->user_id);
+
+    //     if ($user->device_token) {
+    //         $response = Http::post(route('call.pushNotification'), [
+    //             'deviceToken' => $user->device_token,
+    //         ]);
+    //         Log::debug('Notification attempt from status callback:' . $response->body());
+    //         return;
+    //     }
+
+    //     Log::debug('Device token was not found for user_id ' . $user->id);
+    //     return;
+    // }
 }
