295e5e29945173903e04e9cf09ba2ff3bd6904ae
Faiz <iamfaizahmed123@gmail.com>
2023-08-21 03:34:22 +0100

    Update

--- resources/views/stripe-test.blade.php
+++ resources/views/stripe-test.blade.php
@@ -6,24 +6,73 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap');
+
+        ::selection {
+            background: #87CEEB;
+        }
+
+        * {
+            font-family: Poppins, sans-serif;
+        }
+
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #f4f4f4;
+        }
+
+        #container {
+            width: 700px;
+            background-color: #fff;
+            padding: 20px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            border-radius: 5px;
+        }
+
+        label {
+            color: #333;
+            /* A little lighter than black */
+            font-weight: bold;
+            display: block;
+            margin-bottom: 5px;
+        }
+    </style>
 </head>
 
 <body>
 
-    <input id="card-holder-name" type="text">
+    <div id="container">
+        <div>
+            <label for="card-holder-name">Your Full Name:</label>
+            <input id="card-holder-name" type="text"
+                style="padding: 10px; margin: 10px 0; width: 100%; box-sizing: border-box;" placeholder="John Doe">
+        </div>
 
-    <!-- Stripe Elements Placeholder -->
-    <div id="card-element"></div>
+        <div>
+            <label for="amount">Amount:</label>
+            <input id="amount" type="text"
+                style="padding: 10px; margin: 10px 0; width: 100%; box-sizing: border-box;" placeholder="100.00">
+        </div>
 
-    <button id="card-button">
-        Process Payment
-    </button>
+        <!-- Stripe Elements Placeholder -->
+        <div id="card-element"
+            style="padding: 10px; margin: 10px 0; width: 100%; box-sizing: border-box; border: 1px solid #ccc;"></div>
 
-
+        <button id="card-button"
+            style="background-color: #007BFF; color: #fff; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer; margin: 10px 0; display: block; width: 100%; box-sizing: border-box;">
+            Process Payment
+        </button>
+    </div>
     <script src="https://js.stripe.com/v3/"></script>
 
     <script>
-        const stripe = Stripe('pk_test_51JUMhZF43egAbbxbdvc4FIRiALFxHyYECIknypspzMqjYBQ47Kvt8TBY3g44gfhIQHJLPQT4GMwcqlqN1KwKPsbc00UfQoy1mu');
+        const stripe = Stripe(
+            'pk_test_51JUMhZF43egAbbxbdvc4FIRiALFxHyYECIknypspzMqjYBQ47Kvt8TBY3g44gfhIQHJLPQT4GMwcqlqN1KwKPsbc00UfQoy1mu'
+            );
 
         const elements = stripe.elements();
         const cardElement = elements.create('card');
