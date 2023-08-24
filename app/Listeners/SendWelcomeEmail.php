5e6df2ef8913ffd7e94c5003a6af4bf2291e1709
Rana Faiz Ahmad <ranafaizahmad@ranas-mbp.home>
2023-08-24 13:11:50 +0100

    Update

---
+++ app/Listeners/SendWelcomeEmail.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendWelcomeEmail
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(Registered $event): void
+    {
+        Log::debug('Fire the welcome email now.');
+        Log::debug($event->user);
+    }
+}
