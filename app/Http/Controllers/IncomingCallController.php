204f5e89001593d96c429e33d676c415ca1aa45e
Rana Faiz Ahmad <ranafaizahmad@ranas-mbp.home>
2023-08-25 18:29:47 +0100

    Update

--- app/Http/Controllers/IncomingCallController.php
+++ app/Http/Controllers/IncomingCallController.php
@@ -128,7 +128,7 @@ class IncomingCallController extends Controller
             $call_type_id = $availableNumber->call_type_id;
 
             // $twimlBody .= '<Dial callerId="+441156471655" timeout="20"><Client statusCallbackMethod="GET" statusCallbackEvent="initiated ringing answered completed" statusCallback="https://allcalls.io/api/handle-call-status?user_id=' . $user_id . '&call_type_id=' . $call_type_id . '&from=' . $availableNumber->from . '">' . $user_id . '</Client></Dial>';
-            $twimlBody .= '<Dial record="record-from-answer" recordingStatusCallbackEvent="completed" recordingStatusCallback="https://allcalls.io/api/handle-call-recording" callerId="+441156471655" timeout="20"><Client statusCallbackMethod="GET" statusCallbackEvent="initiated ringing answered completed" statusCallback="https://allcalls.io/api/handle-call-status?user_id=' . $user_id . '&amp;call_type_id=' . $call_type_id . '&amp;from=' . $availableNumber->from . '">' . $user_id . '</Client></Dial>';
+            $twimlBody .= '<Dial record="record-from-answer" recordingStatusCallbackMethod="GET" recordingStatusCallbackEvent="completed" recordingStatusCallback="https://allcalls.io/api/handle-call-recording" callerId="+441156471655" timeout="20"><Client statusCallbackMethod="GET" statusCallbackEvent="initiated ringing answered completed" statusCallback="https://allcalls.io/api/handle-call-status?user_id=' . $user_id . '&amp;call_type_id=' . $call_type_id . '&amp;from=' . $availableNumber->from . '">' . $user_id . '</Client></Dial>';
         }
 
         // if (!empty($relevantBids)) {
