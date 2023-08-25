1109fab07738492d5cb93d504741a21e04d7dcd3
Rana Faiz Ahmad <ranafaizahmad@ranas-mbp.home>
2023-08-25 12:36:54 +0100

    Update

--- app/Listeners/ChargeUserForCompletedCall.php
+++ app/Listeners/ChargeUserForCompletedCall.php
@@ -3,6 +3,7 @@
 namespace App\Listeners;
 
 use App\Events\CompletedCallEvent;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -25,5 +26,18 @@ class ChargeUserForCompletedCall
         Log::debug('ChargeUserForCompletedCall');
         Log::debug('User:');
         Log::debug($event->user);
+
+        // Check if the user has sufficient balance
+        if ($event->user->balance >= 5) {
+            // Deduct $5 from the user's balance
+            DB::transaction(function () use ($event) {
+                $event->user->decrement('balance', 5);
+            });
+
+            Log::debug('Deducted $5 from user balance');
+        } else {
+            Log::warning('Insufficient balance to charge for missed call');
+            return;
+        }
     }
 }
