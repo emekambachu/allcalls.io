2dcd2eb78d0921de6c5cdddf4dfd9de494c10c8f
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-08-27 21:52:43 +0100

    Update

--- resources/views/emails/funds_added.blade.php
+++ resources/views/emails/funds_added.blade.php
@@ -1,33 +1,32 @@
 <x-mail::message>
-    # Funds Deposit Confirmation
+# Funds Deposit Confirmation
 
-    **Dear {{ $user->first_name }},**
+**Dear {{ $user->first_name }},**
 
-    We are pleased to confirm that your recent deposit has been successfully processed. Below are the details of your transaction:
+We are pleased to confirm that your recent deposit has been successfully processed. Below are the details of your transaction:
 
-    ## Date of Transaction: {{ now()->format('F j, Y, g:i a') }}
-    
-    ## Account Information
-    **Name:** {{ $user->first_name }} {{ $user->last_name }}  
-    **Email:** {{ $user->email }}  
-    **Contact Number:** {{ $user->contact_number }}
+## Date of Transaction: {{ now()->format('F j, Y, g:i a') }}
 
-    ## Deposit Details
+## Account Information
+**Name:** {{ $user->first_name }} {{ $user->last_name }}  
+**Email:** {{ $user->email }}  
+**Contact Number:** {{ $user->contact_number }}
 
-    @component('mail::table')
-    | Description                   | Amount         |
-    |:------------------------------|---------------:|
-    | Funds Added                   | ${{ number_format($subTotal, 2) }}  |
-    | Processing Fee                | ${{ number_format($processingFee, 2) }} |
-    | ----------------------------- | ---------------|
-    | **Total Amount Deposited**    | **${{ number_format($total, 2) }}** |
-    @endcomponent
+## Deposit Details
 
-    Your account balance is now **${{ number_format($user->balance, 2) }}**.
+@component('mail::table')
+| Description                   | Amount         |
+|:------------------------------|---------------:|
+| Funds Added                   | ${{ number_format($subTotal, 2) }}  |
+| Processing Fee                | ${{ number_format($processingFee, 2) }} |
+| ----------------------------- | ---------------|
+| **Total Amount Deposited**    | **${{ number_format($total, 2) }}** |
+@endcomponent
 
-    If you have any questions or concerns, feel free to reach out to our support team.
+Your account balance is now **${{ number_format($user->balance, 2) }}**.
 
-    **Best regards,**  
-    AllCalls.io
+If you have any questions or concerns, feel free to reach out to our support team.
 
+**Best regards,**  
+AllCalls.io
 </x-mail::message>
