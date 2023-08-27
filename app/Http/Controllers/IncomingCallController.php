2de5a4eae7dfe08613b8b675b1bdfbf0815d74fa
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-08-27 23:09:47 +0100

    Update

--- app/Http/Controllers/IncomingCallController.php
+++ app/Http/Controllers/IncomingCallController.php
@@ -107,6 +107,8 @@ class IncomingCallController extends Controller
 
         $onlineUsers = OnlineUser::prioritizeInternalAgents($onlineUsers);
 
+        $onlineUsers = $onlineUsers->values();
+
         if (!$onlineUsers->count()) {
             Log::debug('No online user found.');
             return '<Response><Say voice="alice" language="en-US">All agents are currently offline. Please try again later.</Say></Response>';
@@ -172,6 +174,8 @@ class IncomingCallController extends Controller
             ->get();
 
         $onlineUsers = OnlineUser::prioritizeInternalAgents($onlineUsers);
+
+        $onlineUsers = $onlineUsers->values();
 
         if (!$onlineUsers->count()) {
             Log::debug('No online user found.');
