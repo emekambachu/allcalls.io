6838372420c85c53ced9a2194ce6048599cba86f
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-08-27 21:43:43 +0100

    Update

--- resources/views/emails/funds_added.blade.php
+++ resources/views/emails/funds_added.blade.php
@@ -1,14 +1,33 @@
 <x-mail::message>
-    Dear {{ $user->first_name }},
-    notify you about your recent deposit fund.
-    @component('mail::table', ['table-header-align' => 'center', 'table-align' => 'right'])
+    # Funds Deposit Confirmation
 
-        | Description | Amount |
-        | --- | --- |
-        | Sub Total | ${{ $subTotal }} |
-        | Credit Card Processing Fee | ${{ $processingFee }} |
-        | --- | --- |
-        | Total | ${{ $total }} |
+    **Dear {{ $user->first_name }},**
 
+    We are pleased to confirm that your recent deposit has been successfully processed. Below are the details of your transaction:
+
+    ## Date of Transaction: {{ now()->format('F j, Y, g:i a') }}
+    
+    ## Account Information
+    **Name:** {{ $user->first_name }} {{ $user->last_name }}  
+    **Email:** {{ $user->email }}  
+    **Contact Number:** {{ $user->contact_number }}
+
+    ## Deposit Details
+
+    @component('mail::table')
+    | Description                   | Amount         |
+    |:------------------------------|---------------:|
+    | Funds Added                   | ${{ number_format($subTotal, 2) }}  |
+    | Processing Fee                | ${{ number_format($processingFee, 2) }} |
+    | ----------------------------- | ---------------|
+    | **Total Amount Deposited**    | **${{ number_format($total, 2) }}** |
     @endcomponent
+
+    Your account balance is now **${{ number_format($user->balance, 2) }}**.
+
+    If you have any questions or concerns, feel free to reach out to our support team.
+
+    **Best regards,**  
+    AllCalls.io
+
 </x-mail::message>
