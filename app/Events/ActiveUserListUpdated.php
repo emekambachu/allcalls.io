eef80c021fe5bab90ab23f3b1afa55e5f991c235
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-08-30 05:06:51 +0100

    Update

--- app/Events/ActiveUserListUpdated.php
+++ app/Events/ActiveUserListUpdated.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class ActiveUserListUpdated
+class ActiveUserListUpdated implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
@@ -32,7 +32,7 @@ class ActiveUserListUpdated
     public function broadcastOn(): array
     {
         return [
-            new Channel('active-user-list-updated'),
+            new Channel('ActiveUserListUpdated'),
         ];
     }
 }
