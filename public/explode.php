3c481c47a17d4c14f1b17c7f2140802a36c6e9f4
urlmixr <urlmixr@users.noreply.github.com>
2023-09-07 14:42:46 -0400

    Create explode.php

---
+++ public/explode.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<script>(function(d, s, id) { var js, pjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//www.tickcounter.com/static/js/loader.js"; pjs.parentNode.insertBefore(js, pjs); }(document, "script", "tickcounter-sdk"));</script><a data-type="countdown" data-id="4434520" class="tickcounter" style="display:block; left:0; width:100%; height:0; position:relative; padding-bottom:25%; margin:0 auto;" title="Time Until World Explodes - Unless App Is Delivered" href="//www.tickcounter.com/">Time Until World Explodes - Unless App Is Delivered</a>
+</body>
+</html>
