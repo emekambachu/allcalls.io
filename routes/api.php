17cffc1209803e92541e757ba2081808e4781f4b
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-09-08 02:05:14 +0100

    Update

--- routes/api.php
+++ routes/api.php
@@ -116,5 +116,5 @@ Route::middleware('auth:sanctum')->get('/callTypes', [CallTypesAPIController::cl
 // Route::patch('/active-users', [ActiveUsersController::class, 'update']);
 Route::post('/active-users-pusher-webhook', [ActiveUsersPusherWebhookController::class, 'store']);
 
-Route::middleware('auth:sanctum')->get('/twilio-ios-access-token-guest', [TwilioIOSAccessTokenGuestController::class, 'show']);
-Route::get('/twilio-ios-access-token', [TwilioIOSAccessTokenController::class, 'show']);
+Route::get('/twilio-ios-access-token-guest', [TwilioIOSAccessTokenGuestController::class, 'show']);
+Route::middleware('auth:sanctum')->get('/twilio-ios-access-token', [TwilioIOSAccessTokenController::class, 'show']);
