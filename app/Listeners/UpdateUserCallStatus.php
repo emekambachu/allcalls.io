92fdcb700c8e9d7b826dbb46da214cbbd44b538f
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-09-10 19:20:12 +0100

    Update user call status and log changes

--- app/Listeners/UpdateUserCallStatus.php
+++ app/Listeners/UpdateUserCallStatus.php
@@ -24,13 +24,24 @@ class UpdateUserCallStatus
     public function handle(CallStatusUpdated $event): void
     {
         $newStatus = $event->info['CallStatus'];
-
         $mappedStatus = $this->mapTwilioStatusToLocalStatus($newStatus);
 
         $user = User::find($event->user->id);
 
         if ($user) {
             $user->update(['call_status' => $mappedStatus]);
+
+            // Log the updated status
+            Log::debug('User call status updated.', [
+                'user_id' => $user->id,
+                'new_status' => $mappedStatus,
+                'twilio_status' => $newStatus,
+            ]);
+        } else {
+            // Optionally, log that the user was not found
+            Log::debug('User not found for status update.', [
+                'user_id' => $event->user->id
+            ]);
         }
     }
 
@@ -41,13 +52,13 @@ class UpdateUserCallStatus
             'ringing' => 'Ringing', // The destination number has started ringing.
             'in-progress' => 'Talking', // The call has been connected, and the connection is currently active.
             'completed' => 'Waiting', // The call was completed (this could map back to waiting)
-    
+
             'busy' => 'Waiting', // Twilio dialed the number, but received a busy response.
             'no-answer' => 'Waiting', // Twilio dialed the number but no one answered before the timeout.
             'canceled' => 'Waiting', // An outbound call was canceled or an incoming call was disconnected.
             'failed' => 'Waiting' // Twilio's carriers could not connect the call.
         ];
-    
+
         return $statusMap[$twilioStatus] ?? 'Waiting';  // default to 'Waiting'
     }
 }
