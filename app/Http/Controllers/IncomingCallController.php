89d0071ba50503fb0c2997255edc1e2133ae7ee2
Muneeb Qadoos <iammmuneeb@gmail.com>
2023-09-11 16:34:12 +0100

    Update

--- app/Http/Controllers/IncomingCallController.php
+++ app/Http/Controllers/IncomingCallController.php
@@ -200,8 +200,8 @@ class IncomingCallController extends Controller
 
         if (!$onlineUsers->count()) {
             Log::debug('No online user found.');
-            return '<Response><Say voice="alice" language="en-US">All agents are currently offline. Please try again later.</Say></Response>';
-        }
+            return '<Response><Dial callerId="+12518626328">+18449831955</Dial></Response>';
+        }        
 
         $onlineUserIds = $onlineUsers->pluck('user_id')->toArray();
         $users = User::whereIn('id', $onlineUserIds)->get();
