a821bcc568638eb36ffe85bf7c953578aadb4e21
Faiz <iamfaizahmed123@gmail.com>
2023-09-25 05:45:56 +0100

    Update

--- resources/js/Pages/Auth/StepsModal.vue
+++ resources/js/Pages/Auth/StepsModal.vue
@@ -176,9 +176,12 @@ let submit = () => {
       <!-- This is the overlay -->
       <div class="relative w-full max-w-md max-h-full mx-auto">
         <div class="relative bg-white rounded-lg shadow-lg">
+          <div class="px-12 py-2">
+            <h1 class="mt-8 text-2xl font-bold">Welcome {{ $page.props.auth.user.first_name }}!</h1>
+          </div>
           <div>
             <!-- Step 0 -->
-            <div v-if="step === 0 && !agentToken" class="pt-6">
+            <div v-if="step === 0 && !agentToken">
               <div class="px-12 py-2">
                 <p class="text-gray-700 text-lg text-left leading-relaxed">
                   Our dynamic bidding system allows you to set a maximum bid for
@@ -187,9 +190,9 @@ let submit = () => {
                 </p>
               </div>
             </div>
-            <div v-if="step === 0 && agentToken" class="pt-6">
+            <div v-if="step === 0 && agentToken">
               <div class="px-12 py-2">
-                <p class="text-gray-700 text-lg text-left leading-relaxed">
+                <p class="text-gray-700 text-sm text-left leading-relaxed">
                   Configure your account to indicate the call types you're
                   interested in.
                 </p>
@@ -197,42 +200,42 @@ let submit = () => {
             </div>
 
             <!-- Step 1 -->
-            <div v-if="step === 1 && !agentToken" class="pt-6">
+            <div v-if="step === 1 && !agentToken">
               <div class="px-12 py-2">
-                <p class="text-gray-700 text-lg text-left leading-relaxed">
+                <p class="text-gray-700 text-sm text-left leading-relaxed">
                   Each call type has a minimum price of $25. Bid higher to
                   increase your chances of receiving a call. The highest bid
                   wins but pays only $1 more than the second-highest bid.
                 </p>
               </div>
             </div>
-            <div v-if="step === 1 && agentToken" class="pt-6">
+            <div v-if="step === 1 && agentToken">
               <div class="px-12 py-2">
-                <p class="text-gray-700 text-lg text-left leading-relaxed">
+                <p class="text-gray-700 text-sm text-left leading-relaxed">
                   You have priority for each call type.
                 </p>
               </div>
             </div>
 
             <!-- Step 2 -->
-            <div v-if="step === 2 && !agentToken" class="pt-6">
+            <div v-if="step === 2 && !agentToken">
               <div class="px-12 py-2">
-                <p class="text-gray-700 text-lg text-left leading-relaxed">
+                <p class="text-gray-700 text-sm text-left leading-relaxed">
                   Select your desired call types and set your maximum price.
                   Ensure you select the states where you're licensed to operate.
                 </p>
               </div>
             </div>
-            <div v-if="step === 2 && agentToken" class="pt-6">
+            <div v-if="step === 2 && agentToken">
               <div class="px-12 py-2">
-                <p class="text-gray-700 text-lg text-left leading-relaxed">
+                <p class="text-gray-700 text-sm text-left leading-relaxed">
                   Choose your call types and verify the states you're licensed
                   in.
                 </p>
               </div>
             </div>
 
-            <div v-if="step === 3" class="pt-6">
+            <div v-if="step === 3">
               <div class="px-12">
                 <div class="mt-4">
                   <GuestInputLabel
@@ -314,7 +317,7 @@ let submit = () => {
               </div>
             </div>
 
-            <div class="px-12 pb-6">
+            <div class="px-12 pb-6 mt-10">
               <div class="flex justify-between">
                 <div class="mt-4">
                   <a
@@ -329,14 +332,15 @@ let submit = () => {
                       viewBox="0 0 24 24"
                       stroke-width="1.5"
                       stroke="currentColor"
-                      class="w-4 h-4 mr-2"
+                      class="w-4 h-4 mr-1"
                     >
                       <path
                         stroke-linecap="round"
                         stroke-linejoin="round"
-                        d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3"
+                        d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
                       />
                     </svg>
+
                     Back</a
                   >
                 </div>
@@ -345,9 +349,24 @@ let submit = () => {
                     v-if="step != 3"
                     type="button"
                     @click.prevent="NextStep"
-                    class="button-custom px-3 py-2 rounded-md"
+                    class="button-custom px-3 py-2 rounded-md flex items-center"
                   >
                     Next
+
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-4 h-4 ml-1"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                      />
+                    </svg>
                   </button>
                   <button
                     @click="submit"
