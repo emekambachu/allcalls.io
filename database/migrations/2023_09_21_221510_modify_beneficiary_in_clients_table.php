14a44145710fb3322083343b243e0aae31add1f0
Muneeb Qadoos <iammmuneeb@gmail.com>
2023-09-28 12:51:33 +0100

    Update

--- database/migrations/2023_09_21_221510_modify_beneficiary_in_clients_table.php
+++ database/migrations/2023_09_21_221510_modify_beneficiary_in_clients_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -11,7 +12,11 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // Set existing NULL values to an empty string
+        DB::table('clients')->whereNull('beneficiary')->update(['beneficiary' => '']);
+
         Schema::table('clients', function (Blueprint $table) {
+            
             $table->string('beneficiary')->default('')->change();
         });
     }
