b0129c199da72fb4981fe0e9c92eece744a9d07b
Faiz <iamfaizahmed123@gmail.com>
2023-10-04 15:40:14 +0100

    Charge user based on highest bid for completed call

--- app/Listeners/ChargeUserForCompletedCall.php
+++ app/Listeners/ChargeUserForCompletedCall.php
@@ -3,6 +3,7 @@
 namespace App\Listeners;
 
 use App\Models\Call;
+use App\Models\Bid;
 use App\Events\FundsTooLow;
 use App\Models\Transaction;
 use App\Events\CompletedCallEvent;
@@ -30,14 +31,43 @@ class ChargeUserForCompletedCall
         Log::debug('User:');
         Log::debug($event->user);
 
+        // Determine if the user is an internal agent
+        $isInternalAgent = $event->user->roles->contains('name', 'internal-agent');
+
+        $chargeAmount = 35; // default value for internal agents
+
+        // If the user is not an internal agent, find their bid for the call type
+        if (!$isInternalAgent) {
+            // Fetch the two highest bids for the call type
+            $topTwoBids = Bid::where('call_type_id', $event->callType->id)
+                ->orderBy('amount', 'desc')
+                ->take(2)
+                ->get();
+                
+            if ($topTwoBids->count() == 2) {
+                // If our user has the highest bid
+                if ($topTwoBids->first()->user_id == $event->user->id) {
+                    $chargeAmount = $topTwoBids->last()->amount + 1; // Charge one more than the second highest bid
+                } else {
+                    // If our user does not have the highest bid, then their charge is their own bid amount
+                    $chargeAmount = $topTwoBids->first()->amount;
+                }
+            } else {
+                Log::warning("Not enough bids found for CallType ID {$event->callType->id}. Defaulting to user's bid amount.");
+                $chargeAmount = $topTwoBids->first()->amount; // Defaulting to the only bid amount available
+            }
+        }
+
+        Log::debug("Charge amount: $$chargeAmount");
+
         // Check if the user has sufficient balance
-        if ($event->user->balance >= 35) {
-            // Deduct $5 from the user's balance
-            DB::transaction(function () use ($event) {
-                $event->user->decrement('balance', 35);
+        if ($event->user->balance >= $chargeAmount) {
+            // Deduct the charge amount from the user's balance
+            DB::transaction(function () use ($event, $chargeAmount) {
+                $event->user->decrement('balance', $chargeAmount);
 
                 Transaction::create([
-                    'amount' => 35,
+                    'amount' => $chargeAmount,
                     'sign' => 0,
                     'user_id' => $event->user->id,
                     'created_at' => now(),
@@ -47,11 +77,11 @@ class ChargeUserForCompletedCall
 
                 $call = Call::where('unique_call_id', $event->uniqueCallId)->first();
                 if ($call) {
-                    $call->amount_spent = 35;
+                    $call->amount_spent = $chargeAmount;
                     $call->save();
                 }
             });
-            Log::debug('Deducted $35 from user balance after completed call');
+            Log::debug("Deducted $$chargeAmount from user balance after completed call");
         } else {
             FundsTooLow::dispatch($event->user);
             Log::warning('Insufficient balance to charge for completed call');
