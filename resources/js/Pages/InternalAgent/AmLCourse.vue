c184c64cad766522e4421d32b5adeba84150212f
Awais Amir <awaisamir23@gmail.com>
2023-10-11 13:48:13 +0500

    pdf view changes

--- resources/js/Pages/InternalAgent/AmLCourse.vue
+++ resources/js/Pages/InternalAgent/AmLCourse.vue
@@ -180,7 +180,7 @@ const fileErrorMessage = ref("Please select a single PDF file.");
     <div class="flex justify-between my-5">
         <div></div>
         <div>
-            <input id="link-checkbox" v-model="form.aml_course" type="checkbox" value=""
+            <input :disabled="page.props.auth.role === 'admin'" id="link-checkbox" v-model="form.aml_course" type="checkbox" value=""
                 class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
             <label for="link-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">I have
                 completed
