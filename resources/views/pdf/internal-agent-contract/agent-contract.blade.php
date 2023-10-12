5cb02098ea681863d16daed54584fa2fc68e8502
Awais Amir <awaisamir23@gmail.com>
2023-10-12 17:26:24 +0500

    signature update

--- resources/views/pdf/internal-agent-contract/agent-contract.blade.php
+++ resources/views/pdf/internal-agent-contract/agent-contract.blade.php
@@ -139,7 +139,7 @@
     @endif
     {{-- End Signature Authorization --}}
     
-    @include('pdf.internal-agent-contract.agent_agency_authorization')
+    {{-- @include('pdf.internal-agent-contract.agent_agency_authorization') --}}
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
