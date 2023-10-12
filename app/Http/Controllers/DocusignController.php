08ad7cdf310cd7155ec5b44a58d80bf07e43e721
awaisamir123 <awaisamir23@gmail.com>
2023-10-12 11:25:07 +0500

    signature-testing

--- app/Http/Controllers/DocusignController.php
+++ app/Http/Controllers/DocusignController.php
@@ -97,6 +97,8 @@ class DocusignController extends Controller
         $decodedData = json_decode($result);
         $request->session()->put('docusign_auth_code', $decodedData->access_token);
 
+        dd($decodedData->access_token);
+
         return redirect()->route('docusign')->with('success', 'Docusign Succesfully Connected');
     }
 
