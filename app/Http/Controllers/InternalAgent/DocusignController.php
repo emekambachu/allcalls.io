df87079f4f3adebd5f1306e3ef9ba18d79be648b
Awais Amir <awaisamir23@gmail.com>
2023-10-12 20:17:23 +0500

    signature-testing

--- app/Http/Controllers/InternalAgent/DocusignController.php
+++ app/Http/Controllers/InternalAgent/DocusignController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\InternalAgent;
 
+use App\Http\Controllers\Controller;
 use Session;
 use Illuminate\Http\Request;
 use DocuSign\eSign\Configuration;
