89e594d8db5476584d96e99181e6bc06264f780e
awaisamir123 <awaisamir23@gmail.com>
2023-10-12 13:22:08 +0500

    signature-testing

--- app/Http/Controllers/InternalAgent/RegistrationStepController.php
+++ app/Http/Controllers/InternalAgent/RegistrationStepController.php
@@ -1609,7 +1609,7 @@ class RegistrationStepController extends Controller
 
         $response = Http::withHeaders([
             'Authorization' => 'Bearer ' . $barerToken,
-        ])->get("https://demo.docusign.net/restapi/v2/accounts/$accountId/envelopes/$envelopeId/documents/$documentId/");
+        ])->get("https://demo.docusign.net/restapi/v2/accounts/$accountId/envelopes/$envelopeId/documents/$documentId");
 
         dd($response, $response->successful(), $response->json(), $response->status());
 
