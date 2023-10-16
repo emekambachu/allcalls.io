831b4aee71a67fbe51c63d6a0e0f5fcacd5ae05c
Faiz <iamfaizahmed123@gmail.com>
2023-10-16 04:11:56 +0100

    Remove commented out code for viewing client in call index view

--- resources/js/Pages/Admin/Calls/Index.vue
+++ resources/js/Pages/Admin/Calls/Index.vue
@@ -213,15 +213,6 @@ let openClientModal = (call) => {
                   <td
                     class="text-gray-700 px-4 py-3 flex items-center justify-end"
                   >
-                    <!-- <button
-                      v-if="call.get_client"
-                      @click="openClientModal(call)"
-                      class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none"
-                      type="button"
-                    >
-                      View Client
-                    </button> -->
-
                     <button
                       class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none"
                       type="button"
