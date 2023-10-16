bf3f2d92a309a51fa4e226c1be529e3171c5c181
awaisamir123 <awaisamir23@gmail.com>
2023-10-16 21:58:35 +0500

    JWT test

--- app/Http/Controllers/InternalAgent/DocusignController.php
+++ app/Http/Controllers/InternalAgent/DocusignController.php
@@ -257,7 +257,7 @@ class DocusignController extends Controller
         $signHere2 = new \DocuSign\eSign\Model\SignHere([
             'anchor_string' => 'SIGNATURE_AUTHORIZATION', // Customize this anchor string
             'anchor_units' => 'pixels',
-            'anchor_y_offset' => '0', // Customize the Y offset
+            'anchor_y_offset' => '-10', // Customize the Y offset
             'anchor_x_offset' => '270', // Customize the X offset
             'optional' => false,
         ]);
@@ -266,7 +266,7 @@ class DocusignController extends Controller
         $signHere3 = new \DocuSign\eSign\Model\SignHere([
             'anchor_string' => 'AGENCY_AUTHORIZATION', // Customize this anchor string
             'anchor_units' => 'pixels',
-            'anchor_y_offset' => '0', // Customize the Y offset
+            'anchor_y_offset' => '-10', // Customize the Y offset
             'anchor_x_offset' => '250', // Customize the X offset
             'optional' => false,
         ]);
