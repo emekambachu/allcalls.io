042dcc03b2a9be0b97eb9b84256169f58f3f523a
Awais Amir <awaisamir23@gmail.com>
2023-10-19 23:11:53 +0500

    invite agent

--- resources/views/emails/agent-invitation.blade.php
+++ resources/views/emails/agent-invitation.blade.php
@@ -1,21 +1,22 @@
 <x-mail::message>
 Welcome to **AllCalls.io**.<br>
 
-This is a inform you that you have been invited as a internal agent at **AllCalls.io** from administration.<br>
+You have been invited to join our company as a **1099** Insurance agent and begin partnering with our insurance carriers. <br>
 
 This link will expire in the next **24 hours**<br>
 
-**Click below to register**<br>
+**Click below to Begin**<br>
 
 @component('mail::button', ['url' => $url])
 Click Here
 @endcomponent
 <br>
-If above link look like expired then contact with our support to reinvite you.<br>
+If the above link is expired, please contact our support team or the person who invited you, to resend the link.<br>
 
-Our dedicated support team is always on hand to assist you, should you have any questions or need help along the way. Remember, at AllCalls.io, we're all about fostering connections that matter.<br>
+Our dedicated support team is always on hand to assist you, should you have any questions or need help along the way. Remember, at AllCalls.io, your success is our priority!<br>
 
-Thank you for choosing us to be a part of your journey. Let's redefine insurance call experiences together!<br>
+Thank you for choosing us to be a part of your journey. Let's redefine the insurance industry together!
+<br>
 
 Warm regards,<br>
 
@@ -23,5 +24,5 @@ The AllCalls.io Team<br>
 
 ---
 
-P.S. If you have any questions or need assistance, don't hesitate to reach out to our support team at [support@allcalls.io](mailto:support@allcalls.io) or visit our [Help Center](http://help.allcalls.io). We're here to help!
+P.S. If you have any questions or need assistance, don't hesitate to reach out to our support team at support@allcalls.io or visit our Help Center. We're here to help!
 </x-mail::message>
