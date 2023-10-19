3b4183aa7bfd35863a2c17edd5ecb917f5cffa86
awaisamir123 <awaisamir23@gmail.com>
2023-10-20 02:49:26 +0500

    signature testing

--- database/migrations/2023_10_19_092957_add_email_in_agent_invites.php
+++ database/migrations/2023_10_19_092957_add_email_in_agent_invites.php
@@ -4,17 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table('agent_invites', function (Blueprint $table) {
-            $table->string('email')->unique()->after('id');
-            $table->string('url')->after('token');
-        });
+        if (!Schema::hasColumn('email', 'url')) {
+            Schema::table('agent_invites', function (Blueprint $table) {
+                $table->string('email')->unique()->after('id');
+                $table->string('url')->after('token');
+            });
+        }
     }
 
     /**
