da774380c4d54068541609b181fa6b27c80d53da
Muneeb Qadoos <iammmuneeb@gmail.com>
2023-10-23 20:56:51 +0100

    Update

--- app/Http/Controllers/CallStatusController.php
+++ app/Http/Controllers/CallStatusController.php
@@ -71,6 +71,23 @@ class CallStatusController extends Controller
 
             case 'no-answer':
                 Log::debug('no-answer event for user ' . $request->user_id);
+                
+                $call = $user->call;
+                if (!$call) {
+                    // Handle the case where there's no Call associated with the user.
+                    return response()->json(['message' => 'Call not found for the user'], 404);
+                }
+                // 3. Fetch the `created_at` column of that `Call` model.
+                $createdAt = $call->created_at;
+                // 4. Calculate the difference between the current time and the `Call` model's `created_at`.
+                $now = now();
+                $difference = $now->diff($createdAt);
+                // 5. Display the result in a time format.
+                $timeDifference = $difference->format('%y years, %m months, %d days, %h hours, %i minutes, %s seconds');
+                // Return the result
+                // return response()->json(['time_difference' => $timeDifference]);
+                Log::debug('Time difference: ' . $timeDifference);
+                
                 // Dispatch MissedCallEvent
                 MissedCallEvent::dispatch($user);
                 break;
