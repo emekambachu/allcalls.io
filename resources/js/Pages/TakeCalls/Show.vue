31235d4869376bfc8e78d9e4aef44497632300c0
iammmuneeb <iammmuneeb@gmail.com>
2023-10-25 22:36:29 +0100

    Update

--- resources/js/Pages/TakeCalls/Show.vue
+++ resources/js/Pages/TakeCalls/Show.vue
@@ -157,7 +157,7 @@ watchEffect(async () => {
             <li v-for="callType in callTypesWithToggles" :key="callType.callType.id" class="py-3 sm:py-4">
               <div class="flex justify-end items-center">
                 <div class="text-md text-gray-800">Turn calls on <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                    stroke-width="1.5" stroke="currentColor" class="w-4 h-4 relative my-1" style="left: 30px">
+                    stroke-width="1.5" stroke="currentColor" class="w-4 h-4 relative my-1" style="left: 35px">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 15l-6 6m0 0l-6-6m6 6V9a6 6 0 0112 0v3" />
                   </svg>
                 </div>
