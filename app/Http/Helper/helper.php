01406f31b14390e98342161d7f4fa47308ae2d34
awaisamir123 <awaisamir23@gmail.com>
2023-10-26 20:40:51 +0500

    PDF required removed. Status list change

--- app/Http/Helper/helper.php
+++ app/Http/Helper/helper.php
@@ -1,20 +1,24 @@
 <?php
 
 const PROGRESS_STATUSES = [
-    'Invite sent',
-    'Started contracting',
-    'Completed and uploaded documents',
-    'Onboarding documents reviewed',
-    'Carrier contracts sent',
-    'Agent signed',
+//    'Invite sent',
+//    'Started contracting',
+//    'Completed and uploaded documents',
+//    'Onboarding documents reviewed',
+//    'Carrier contracts sent',
+//    'Agent signed',
+    'Needs ICA',
+    'Contracting Reviewing Documents',
+    'Contracting Missing AML',
+    'Contracting Missing Resident License',
+    'Contracting Missing Banking Info',
+    'Carrier Contracts Sent to Agent',
+    'Agent Signed Carrier Contracts',
+    'Contracts Sent to Carrier',
 ];
-
-
 
 function getStateName($id)
 {
-
     $state = \App\Models\State::find($id);
     return $state->full_name;
-
 }
