cf1679fa1ad5c76a55581c0610d96ed715760d4c
awaisamir123 <awaisamir23@gmail.com>
2023-10-27 19:32:24 +0500

    fixes

--- resources/js/Pages/Admin/Agent/ApproveConfirm.vue
+++ resources/js/Pages/Admin/Agent/ApproveConfirm.vue
@@ -105,10 +105,6 @@ let close = () => {
                         <div class=" mb-3">
                             <div class="flex justify-end">
                                 <div class="mt-4">
-                                    <button @click="onCancel" class="button-custom-back mr-3 px-3 py-2 rounded-md">
-                                        Cancel</button>
-                                </div>
-                                <div class="mt-4">
                                     <button type="button" @click="onApprove"
                                         :class="{ 'opacity-25': isLoading }"
                                         :disabled="isLoading"
