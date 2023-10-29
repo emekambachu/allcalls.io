13cc8c8254f4943760a6264e3ea4d8d1045978e2
iammmuneeb <iammmuneeb@gmail.com>
2023-10-29 23:36:24 +0000

    Update

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -259,7 +259,7 @@ onMounted(() => {
   Echo.private(`${page.props.auth.user.id}.notifications`)
     .listen('CallAcceptedOrRejected', e => {
       console.log('call accepted or rejected by one of the phone devices');
-      conosle.log(e);
+      console.log(e);
       showRinging.value = false;
       showOngoing.value = false;
     });
