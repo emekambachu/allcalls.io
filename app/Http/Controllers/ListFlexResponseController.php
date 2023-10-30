5c61ef789bb738b86627e240cb230303f105e92a
iammmuneeb <iammmuneeb@gmail.com>
2023-10-30 22:50:48 +0000

    Update

--- app/Http/Controllers/ListFlexResponseController.php
+++ app/Http/Controllers/ListFlexResponseController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class ListFlexResponseController extends Controller
 {
     public function store(Request $request)
     {
+        Log::debug('api-logs:listflex-response: Received request.');
+
         // 1. Get the input parameters from the incoming request.
         $firstname = $request->input('firstname');
         $lastname  = $request->input('lastname');
@@ -26,7 +28,7 @@ class ListFlexResponseController extends Controller
             'mobile'     => $mobile,
         ];
 
-        Log::debug('Payload: ', $payload);
+        Log::debug('api-logs:listflex-response: Prepared payload: ' . json_encode($payload));
 
         // 3. Make the HTTP request to the endpoint.
         $response = Http::withHeaders([
@@ -34,10 +36,16 @@ class ListFlexResponseController extends Controller
             'Content-Type' => 'application/json',
         ])->post('https://api-mme-bpo.benefitalign.com/ui/vendorleads', $payload);
 
+        if ($response->failed()) {
+            Log::debug('api-logs:listflex-response: Failed API request. Response: ' . $response->body());
+        }
+
         // Handle the response based on the Status
         if ($response->json('Status') === 'Success') {
+            Log::debug('api-logs:listflex-response: API request successful.');
             return response()->json(['message' => 'Success']);
         } else {
+            Log::debug('api-logs:listflex-response: API request returned a non-success status.');
             return response()->json(['message' => 'Failure'], 400);
         }
     }
