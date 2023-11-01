8cd516d7e745f7c4f69c8065b0f7dac8f2fc64ec
Muneeb Qadoos <iammmuneeb@gmail.com>
2023-11-01 05:43:51 +0000

    Update

---
+++ app/Http/Controllers/IOSVersionCheckController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IOSVersionCheckController extends Controller
+{
+    //
+    public function checkVersion()
+    {
+        return response()->json([
+            'latest_version' => '1.0.2'
+        ]);
+    }
+}
