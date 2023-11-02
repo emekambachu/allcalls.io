a8e29e10517de118206a041041cf28fb2852aeb6
iammmuneeb <iammmuneeb@gmail.com>
2023-11-02 17:52:42 +0000

    Refactor API response to return only available agents

--- app/Http/Controllers/AvailableAgentsAPIController.php
+++ app/Http/Controllers/AvailableAgentsAPIController.php
@@ -14,6 +14,8 @@ class AvailableAgentsAPIController extends Controller
 
         Log::debug('api-logs:available-agents:response-body', ['responseBody' => $response->body()]);
 
-        return $response->body();
+        return [
+            'available_agents' => json_decode($response->body())->purpose_health_available_agents
+        ];
     }
 }
