650f273c10ac53cb231181e9b70bc14c461ed910
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-11-06 19:50:58 +0000

    Send push notification to custom API

--- app/Notifications/MissedCall.php
+++ app/Notifications/MissedCall.php
@@ -3,9 +3,10 @@
 namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Notifications\Notification;
 
 class MissedCall extends Notification
 {
@@ -50,5 +51,54 @@ class MissedCall extends Notification
         return [
             'message' => 'You missed a call from a client.'
         ];
+    }
+
+
+    /**
+     * Send the array representation of the notification to a custom API.
+     *
+     * @param object $notifiable
+     * @return void
+     */
+    public function toPushNotification(object $notifiable)
+    {
+        // Assuming $notifiable->device_token is where the device token is stored
+        $deviceToken = $notifiable->device_token;
+        
+        $notificationData = $this->toArray($notifiable);
+        
+        $serverKey = env('PUSH_TEST_SERVER_KEY');
+
+        $notification = [
+            'to' => $deviceToken,
+            'notification' => [
+                'title' => $notificationData['title'],
+                'body' => $notificationData['message'],
+            ],
+            'android' => [
+                'direct_boot_ok' => true,
+            ],
+        ];
+
+        $headers = [
+            'Authorization: key=' . $serverKey,
+            'Content-Type: application/json',
+        ];
+
+        $ch = curl_init('https://fcm.googleapis.com/fcm/send');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($notification));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($ch);
+
+        if ($response === false) {
+            Log::error('Error sending push notification: ' . curl_error($ch));
+        } else {
+            Log::info('Push notification sent: ' . $response);
+        }
+
+        curl_close($ch);
     }
 }
