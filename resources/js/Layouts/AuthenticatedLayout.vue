eeba47df94ced2a8dafbd85e2ae1ce5df157c880
iammmuneeb <iammmuneeb@gmail.com>
2023-11-06 16:48:09 +0000

    Remove download icon from AuthenticatedLayout.vue

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -540,14 +540,14 @@ let appDownloadModal = ref(false);
 
               <div class="hidden md:flex md:self-start sm:pt-6 sm:ml-6">
                 <div class="flex items-center">
-                  <div>
+                  <!-- <div>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                       stroke="currentColor" class="w-6 h-6">
                       <path stroke-linecap="round" stroke-linejoin="round"
                         d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
                     </svg>
 
-                  </div>
+                  </div> -->
                   <div>
                     <Link href="/billing/funds"
                       class="mr-3 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
