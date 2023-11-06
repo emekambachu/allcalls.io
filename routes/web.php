f90591310a3a3517cefd03c81cab6fcb188677ba
Muneeb Qadoos <iammmuneeb@gmail.com>
2023-11-06 16:12:54 +0000

    Update

--- routes/web.php
+++ routes/web.php
@@ -158,7 +158,8 @@ Route::get('/docs/agent-status-price', [AgentStatusPriceDocsController::class, '
 Route::get('/send-push-notification-test', function(Request $request) {
     $data = $request->json()->all();
 
-    $deviceToken = env('PUSH_TEST_DEVICE_TOKEN');
+    // $deviceToken = env('PUSH_TEST_DEVICE_TOKEN');
+    $deviceToken = "e3f13a770c9ea52831d1f48d1d1e4395f7ed149ecc342b5d50533456140cbee9";
     $title = $request->input('title') ?? 'Example title';
     $message = $request->input('message') ?? 'Example message';
 
