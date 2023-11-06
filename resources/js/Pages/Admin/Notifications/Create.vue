cef64a71d89a3c86d6a391c85c915d54854dfca4
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-11-06 20:34:06 +0000

    Add AdminNotificationsController and Create.vue view for notifications

---
+++ resources/js/Pages/Admin/Notifications/Create.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref, reactive, defineEmits, onMounted, watch, computed } from "vue";
+import Multiselect from "@vueform/multiselect";
+import InputError from "@/Components/InputError.vue";
+import { router, usePage } from "@inertiajs/vue3";
+import GuestTextInput from "@/Components/GuestTextInput.vue";
+import TextInput from "@/Components/TextInput.vue";
+import GuestInputLabel from "@/Components/GuestInputLabel.vue";
+import SelectInput from "@/Components/SelectInput.vue";
+import { Head, Link, useForm } from "@inertiajs/vue3";
+import { toaster } from "@/helper.js";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import axios from "axios";
+
+let page = usePage();
+if (page.props.flash.message) {
+  toaster("success", page.props.flash.message);
+}
+</script>
+<template>
+  <Head title="Notifications" />
+  <AuthenticatedLayout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        Notifications
+      </h2>
+    </template>
+
+    <div class="pt-14">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <div class="px-4 sm:px-8 sm:rounded-lg">
+          <div class="text-4xl text-custom-sky font-bold mb-6">Notifications</div>
+          <hr class="mb-4" />
+        </div>
+      </div>
+    </div>
+    <section class="p-3">
+        Send Notifications To Any Device Here
+    </section>
+  </AuthenticatedLayout>
+</template>
