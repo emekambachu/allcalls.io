bc668b8235eb2758b3abfdf4c3d32434fea48de9
Muneeb Qadoos <iammmuneeb@gmail.com>
2023-11-07 03:42:05 +0000

    Update

--- resources/js/Pages/Admin/Notifications/Create.vue
+++ resources/js/Pages/Admin/Notifications/Create.vue
@@ -16,44 +16,44 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
 
 const { users } = usePage().props;
 const selectedUserId = ref('');
+const selectedDevices = ref([]);
 
 // Computed property to get devices for the selected user
 const selectedUserDevices = computed(() => {
-  const user = users.find(user => user.id === selectedUserId.value);
+  const user = users.find(u => u.id === selectedUserId.value);
   return user ? user.devices : [];
 });
 
-
-// Create a reactive form object
+// Reactive form object
 const form = useForm({
   title: '',
-  message: ''
+  message: '',
+  user_id: '',
+  devices: []
 });
 
 function sendPushNotification() {
-  // Disable the button to prevent multiple submissions
-  form.processing = true;
+  form.user_id = selectedUserId.value;
+  form.devices = selectedDevices.value.map(device => device.fcm_token);
 
-  // Post the form data
   form.post('/send-push-notification-test', {
-    data: {
-      user_id: selectedUserId.value,
-      devices: selectedDevices.value.map(device => device.fcm_token),
-      title: form.title,
-      message: form.message
-    },
     onSuccess: () => {
       toaster("success", "Notification sent successfully!");
-      form.reset(); // Reset the form after successful submission
+      form.reset(); // Reset the form fields
     },
-    onError: () => {
+    onError: (errors) => {
       toaster("error", "Failed to send notification.");
-    },
-    onFinish: () => {
-      form.processing = false; // Re-enable the button after the request
+      // Handle form errors if needed
     }
   });
 }
+
+// Watch the selectedUserId to update the devices array
+watch(selectedUserId, (newVal, oldVal) => {
+  if (newVal !== oldVal) {
+    selectedDevices.value = []; // Clear the selected devices when a new user is selected
+  }
+});
 
 let page = usePage();
 
@@ -61,70 +61,62 @@ if (page.props.flash.message) {
   toaster("success", page.props.flash.message);
 }
 </script>
+
 <template>
-  <Head title="Notifications" />
+  <Head title="Send Push Notifications" />
   <AuthenticatedLayout>
     <template #header>
-      <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-        Notifications
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Send Push Notifications
       </h2>
     </template>
 
-    <div class="pt-14">
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-        <div class="px-4 sm:px-8 sm:rounded-lg">
-          <div class="text-4xl text-custom-sky font-bold mb-6">Notifications</div>
-          <hr class="mb-4" />
-        </div>
-      </div>
-    </div>
-    <section class="px-16">
-        Send Notifications To Any Device Here
-
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-          <!-- <ul>
-            <li v-for="device in devices" :key="device.id">
-              {{ device.user_id }} - {{ device.device_type }}
-            </li>
-          </ul> -->
-
-          <!-- User Selection -->
-          <select v-model="selectedUserId">
-            <option value="">Select a user</option>
+    <section class="py-8">
+      <div class="container mx-auto px-4">
+        <!-- User Selection -->
+        <div class="mb-4">
+          <InputLabel for="user" value="Select User:" />
+          <select v-model="selectedUserId" class="w-full p-2 border rounded">
+            <option disabled value="">Select a user</option>
             <option v-for="user in users" :key="user.id" :value="user.id">
-              {{ user.name }}
+              {{ user.email }}
             </option>
           </select>
-          
-          <!-- Devices List -->
-          <div v-if="selectedUserId">
-            <h3>Select a Device</h3>
-            <ul>
-              <li v-for="device in selectedUserDevices" :key="device.id">
-                {{ device.name }} - {{ device.fcm_token }}
-              </li>
-            </ul>
-          </div>
-
         </div>
-
-        <div class="max-w-xl">
-          <form @submit.prevent="sendPushNotification">
-            <div class="mt-4">
-              <InputLabel for="title" value="Push Title:" />
-              <TextInput id="title" type="text" class="mt-1" v-model="form.title" required autofocus />
-            </div>
-
-            <div class="mt-4">
-              <InputLabel for="message" value="Push Description" />
-              <TextInput id="message" type="text" class="mt-1" v-model="form.message" required autofocus />
-            </div>
-
-            <div class="mt-6">
-              <PrimaryButton type="submit" :disabled="form.processing">Send Notification</PrimaryButton>
-            </div>
-          </form>
+        
+        <!-- Devices List -->
+        <div v-if="selectedUserDevices.length > 0" class="mb-4">
+          <h3 class="font-semibold mb-2">Select Devices:</h3>
+          <ul>
+            <li v-for="device in selectedUserDevices" :key="device.id" class="mb-2">
+              <label class="inline-flex items-center">
+                <input type="checkbox" :value="device" v-model="selectedDevices" class="form-checkbox">
+                <span class="ml-2">
+                  {{ device.device_type }} 
+                  <!-- ({{ device.fcm_token }}) -->
+                </span>
+              </label>
+            </li>
+          </ul>
         </div>
+        
+        <!-- Notification Form -->
+        <div class="mt-4">
+          <InputLabel for="title" value="Notification Title:" />
+          <TextInput id="title" type="text" v-model="form.title" required autofocus class="w-full p-2 border rounded" />
+        </div>
+        
+        <div class="mt-4">
+          <InputLabel for="message" value="Notification Message:" />
+          <TextInput id="message" type="text" v-model="form.message" required class="w-full p-2 border rounded" />
+        </div>
+        
+        <div class="mt-6">
+          <PrimaryButton @click="sendPushNotification" :disabled="form.processing" class="px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded">
+            Send Notification
+          </PrimaryButton>
+        </div>
+      </div>
     </section>
   </AuthenticatedLayout>
 </template>
