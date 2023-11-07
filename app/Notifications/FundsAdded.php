263fc9008f4538bf112f913720db03b7bd6d6a8e
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-11-07 01:42:19 +0000

    Add FundsAdded notification class

---
+++ app/Notifications/FundsAdded.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use App\Notifications\PushChannel;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class FundsAdded extends Notification
+{
+    use Queueable;
+
+    public $amount;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($amount)
+    {
+        $this->amount = $amount;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database', PushChannel::class];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'message' => $this->amount . ' added to your funds.',
+            'amount' => $this->amount,
+        ];
+    }
+
+
+    /**
+     * Send the array representation of the notification to a custom API.
+     *
+     * @param object $notifiable
+     * @return void
+     */
+    public function toPush(object $notifiable)
+    {
+        return $this->toArray($notifiable);
+    }
+}
