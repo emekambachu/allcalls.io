e7f5ebeccb0e14a385b1cf68be72756865cc5e5f
Muneeb Qadoos <iammmuneeb@gmail.com>
2023-11-12 23:10:40 +0000

    Update

--- resources/js/Pages/Admin/Notifications/Create.vue
+++ resources/js/Pages/Admin/Notifications/Create.vue
@@ -96,7 +96,7 @@ if (page.props.flash.message) {
             v-model="selectedUser" 
             :options="formattedUsers"
             label="fullNameWithEmail"
-            track-by="formattedUsers" 
+            track-by="users" 
             :searchable="true"
             :allow-empty="false"
           />
