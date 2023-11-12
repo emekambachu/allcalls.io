a4c398f6f5e049852314795f9c1be9b8ee3849c9
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-11-12 22:31:54 +0000

    Update

--- app/Http/Controllers/EquisAPIController.php
+++ app/Http/Controllers/EquisAPIController.php
@@ -12,7 +12,7 @@ class EquisAPIController extends Controller
     {
         $clientId = env('EQUIS_CLIENT_ID'); // Your client ID here
         $clientSecret = env('EQUIS_CLIENT_SECRET'); // Your client secret here
-        $url = 'https://equisapipartner-uat.azurewebsites.net/'; // The specific endpoint you want to access
+        $url = 'https://equisapipartner-uat.azurewebsites.net/Agent'; // The specific endpoint you want to access
 
         // First, retrieve the Bearer token
         $tokenResponse = Http::asForm()->post('https://equisfinancialb2c.b2clogin.com/equisfinancialb2c.onmicrosoft.com/B2C_1_SignIn/oauth2/v2.0/token', [
