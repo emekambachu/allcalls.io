f241ac47c5708e26990046d0e6c605c3ceb3bdc1
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-11-13 02:05:05 +0000

    Update uplineAgentEFNumber value in EquisAPIController.php

--- app/Http/Controllers/EquisAPIController.php
+++ app/Http/Controllers/EquisAPIController.php
@@ -51,7 +51,7 @@ class EquisAPIController extends Controller
                 "role" => "Agent",
                 "state" => "NC",
                 "suffix" => "Jr",
-                "uplineAgentEFNumber" => "EF222171",
+                "uplineAgentEFNumber" => "222171",
                 "zipCode" => "28801"
             ]);
 
