cbb5981883e6473ce70fe80c393a1b792fcdd639
awaisamir123 <awaisamir23@gmail.com>
2023-11-16 22:36:41 +0500

    ssn placeholder

--- resources/js/Pages/InternalAgent/ContactDetail.vue
+++ resources/js/Pages/InternalAgent/ContactDetail.vue
@@ -215,6 +215,7 @@ let formatSSN = (event) => {
                     Security Number (SSN)<span class="text-red-500">*</span></label>
                 <input type="text" @input="formatSSN" v-model="form.ssn" id="default-input"
                     :disabled="page.props.auth.role === 'admin'"
+                    placeholder="xxx-xx-xxxx"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:text-white">
                 <div v-if="firstStepErrors.ssn" class="text-red-500" v-text="firstStepErrors.ssn[0]"></div>
             </div>
