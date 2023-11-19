80422399434931bb4d77121c90040a7625e3972f
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-11-19 21:11:50 +0000

    Update AuthenticatedLayout.vue with new SVG icon and mobile notifications functionality

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -642,7 +642,21 @@ let appDownloadModal = ref(false);
               :href="route('admin.agent-invites.index')"
               :active="route().current('admin.agent-invites.index')"
             >
-              <img src="/img/clients.png" alt="" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-8 h-8 mr-2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+                />
+              </svg>
+
               Agent Invites
             </NavLink>
             <NavLink
@@ -1309,7 +1323,12 @@ let appDownloadModal = ref(false);
               <div
                 class="w-full rounded-md bg-custom-indigo shadow-lg focus:outline-none"
               >
-                <div class="text-lg font-bold px-3 py-2 text-white" @click.prevent="showMobileNotifications = !showMobileNotifications">Notifications</div>
+                <div
+                  class="text-lg font-bold px-3 py-2 text-white"
+                  @click.prevent="showMobileNotifications = !showMobileNotifications"
+                >
+                  Notifications
+                </div>
 
                 <!-- Unread Notifications -->
                 <div v-if="showMobileNotifications && unreadNotifications.length">
@@ -1354,7 +1373,13 @@ let appDownloadModal = ref(false);
                 </div>
 
                 <!-- All Notifications -->
-                <div v-if="showMobileNotifications && userNotifications && userNotifications.length">
+                <div
+                  v-if="
+                    showMobileNotifications &&
+                    userNotifications &&
+                    userNotifications.length
+                  "
+                >
                   <div
                     class="px-3 py-2 text-sm text-white font-bold flex items-center justify-between"
                   >
