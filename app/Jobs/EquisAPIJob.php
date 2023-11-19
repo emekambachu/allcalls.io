7e6bd3d9d6f65f63897e570ad1bc5afeae07c300
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-11-19 03:53:56 +0000

    Update

--- app/Jobs/EquisAPIJob.php
+++ app/Jobs/EquisAPIJob.php
@@ -102,9 +102,9 @@ class EquisAPIJob implements ShouldQueue
     protected function sendEmailsToPeople()
     {
         Log::debug('equis-api-job:Sending email to people');
-        Mail::to(['bizdev@equisfinancial.com'])
-            ->cc(['contracting@allcalls.io'])
-            ->send(new EquisDuplicateMail($this->user->internalAgentContract->first_name . " " . $this->user->internalAgentContract->last_name, 'EF222171', $this->user->internalAgentContract->email));
+        // Mail::to(['bizdev@equisfinancial.com'])
+        //     ->cc(['contracting@allcalls.io'])
+        //     ->send(new EquisDuplicateMail($this->user->internalAgentContract->first_name . " " . $this->user->internalAgentContract->last_name, 'EF222171', $this->user->internalAgentContract->email));
 
         Mail::to(['iamfaizahmed123@gmail.com'])
             ->send(new EquisDuplicateMail($this->user->internalAgentContract->first_name . " " . $this->user->internalAgentContract->last_name, 'EF222171', $this->user->internalAgentContract->email));
