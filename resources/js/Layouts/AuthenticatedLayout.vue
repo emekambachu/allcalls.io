106c3faa8ca4ce823204d3f76e3728c99fa7a1a7
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-11-19 21:23:34 +0000

    Update

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -720,7 +720,21 @@ let appDownloadModal = ref(false);
               :href="route('admin.available-number.index')"
               :active="route().current('admin.available-number.index')"
             >
-              <img src="/img/clients.png" alt="" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-8 h-8 mr-2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
+                />
+              </svg>
+
               Available Numbers
             </NavLink>
 
