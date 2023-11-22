8f67551e736afe83d7ba827149c28337e403eae2
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-11-22 23:32:36 +0000

    Update

---
+++ resources/js/Pages/Admin/Calls/IndexNew.vue
@@ -0,0 +1,161 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head, router, usePage } from "@inertiajs/vue3";
+import { toaster } from "@/helper.js";
+
+let page = usePage();
+if (page.props.flash.message) {
+  toaster("success", page.props.flash.message);
+}
+let props = defineProps(["calls"]);
+
+console.log(props);
+
+let paginate = (url) => {
+  router.visit(url);
+};
+</script>
+
+<style scoped>
+/* Keep existing styles or adjust as needed for the User Activities page. */
+</style>
+
+<template>
+  <Head title="Calls" />
+  <AuthenticatedLayout>
+    <div class="pt-14 flex justify-between px-16">
+      <div>
+        <div class="text-4xl text-custom-sky font-bold mb-6">Calls</div>
+      </div>
+
+      <div class="flex items-center">
+        <!-- <button class="button-custom-back px-3 py-2 rounded-md mr-2">Clear All</button> -->
+      </div>
+    </div>
+
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+      <div class="px-4 sm:px-8 sm:rounded-lg">
+        <hr class="mb-4" />
+      </div>
+    </div>
+
+    <section class="py-3 sm:py-5">
+      <div class="px-4 mx-auto max-w-screen-2xl lg:px-12">
+        <div
+          class="relative overflow-hidden bg-white sm:rounded-lg"
+        >
+          <div
+            class="flex flex-col px-4 py-3 space-y-3 lg:flex-row lg:items-center lg:justify-between lg:space-y-0 lg:space-x-4"
+          >
+            <div class="flex items-center flex-1 space-x-4">
+              <h5>
+                <span class="text-gray-500">All Calls:</span>
+                <span class="">123456</span>
+              </h5>
+              <h5>
+                <span class="text-gray-500">Total Revenue:</span>
+                <span class="">$88.4k</span>
+              </h5>
+            </div>
+            <div
+              class="flex flex-col flex-shrink-0 space-y-3 md:flex-row md:items-center lg:justify-end md:space-y-0 md:space-x-3"
+            >
+              <button
+                type="button"
+                class="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 focus:outline-none"
+              >
+                <svg
+                  class="h-3.5 w-3.5 mr-2"
+                  fill="currentColor"
+                  viewbox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    clip-rule="evenodd"
+                    fill-rule="evenodd"
+                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                  />
+                </svg>
+                Add new product
+              </button>
+              <button
+                type="button"
+                class="flex items-center justify-center flex-shrink-0 px-3 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200"
+              >
+                <svg
+                  class="w-4 h-4 mr-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  fill="none"
+                  viewbox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+                  />
+                </svg>
+                Update stocks 1/250
+              </button>
+              <button
+                type="button"
+                class="flex items-center justify-center flex-shrink-0 px-3 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200"
+              >
+                <svg
+                  class="w-4 h-4 mr-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewbox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+                  />
+                </svg>
+                Export
+              </button>
+            </div>
+          </div>
+          <div v-if="calls.data.length" class="overflow-x-auto">
+            <table class="w-full text-sm text-left text-gray-500">
+              <thead
+                class="text-xs text-gray-700 uppercase bg-gray-50"
+              >
+                <tr>
+                  <th scope="col" class="px-4 py-3">ID</th>
+                  <th scope="col" class="px-4 py-3">User ID</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  class="border-b hover:bg-gray-100"
+                  v-for="call in calls"
+                  :key="call.id"
+                >
+                  <td
+                    class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {{ call.id }}
+                  </td>
+                  <td
+                    class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {{ call.user_id }}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </section>
+  </AuthenticatedLayout>
+</template>
