25982f072de2299a8a027deabd4edd274534b75a
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-11-22 19:43:00 +0000

    Update agent level icon size in AuthenticatedLayout.vue

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -563,7 +563,7 @@ let appDownloadModal = ref(false);
             <NavLink class="mb-10 gap-2" :href="route('admin.internal.agent.level.index')"
               :active="route().current('admin.internal.agent.level.index')">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-6 h-6">
+                stroke="currentColor" class="w-8 h-8 mr-2">
                 <path stroke-linecap="round" stroke-linejoin="round"
                   d="M6 13.5V3.75m0 9.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 9.75V10.5" />
               </svg>
