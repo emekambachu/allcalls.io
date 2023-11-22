26d4fbe47a65dd748cf383a0c935f40d1aa6a9e5
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-11-22 21:00:40 +0000

    Remove duplicate code for "Agent Invites" NavLink

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -536,6 +536,18 @@ let appDownloadModal = ref(false);
 
               Customers
             </NavLink>
+
+            <NavLink class="mb-10 gap-2" :href="route('admin.agent-invites.index')"
+              :active="route().current('admin.agent-invites.index')">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-8 h-8 mr-2">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                  d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+              </svg>
+
+              Agent Invites
+            </NavLink>
+
             <NavLink class="mb-10 gap-2" :href="route('admin.agent.index')" :active="route().current('admin.agent.index') ||
               route().current('admin.agent.detail')
               ">
@@ -592,16 +604,7 @@ let appDownloadModal = ref(false);
             </NavLink>
 
 
-            <NavLink class="mb-10 gap-2" :href="route('admin.agent-invites.index')"
-              :active="route().current('admin.agent-invites.index')">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-8 h-8 mr-2">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
-              </svg>
 
-              Agent Invites
-            </NavLink>
             <NavLink class="mb-10 gap-2" :href="route('admin.available-number.index')"
               :active="route().current('admin.available-number.index')">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
