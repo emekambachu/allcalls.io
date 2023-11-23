fa26a08dde934a69798292c9c32c32fb1e65f58f
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-11-23 01:10:32 +0000

    Update

--- resources/js/Pages/Admin/Calls/IndexNew.vue
+++ resources/js/Pages/Admin/Calls/IndexNew.vue
@@ -115,13 +115,30 @@ let paginate = (url) => {
 
                   <PopoverPanel class="absolute z-10 w-40 -left-20">
                     <div class="border border-gray-100 p-2 shadow bg-white mt-2">
-                      <div class="flex">
-                        <div><input type="checkbox"></div>
-                        <div>ID</div>
+                      <div class="flex items-center mb-4">
+                        <input
+                          checked
+                          type="checkbox"
+                          value=""
+                          class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                        />
+                        <label
+                          class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                          >ID</label
+                        >
                       </div>
-                      <div class="flex">
-                        <div><input type="checkbox"></div>
-                        <div>Call Taken</div>
+                      <div class="flex items-center">
+                        <input
+                          checked
+                          type="checkbox"
+                          value=""
+                          class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                        />
+                        <label
+                          for="checked-checkbox"
+                          class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                          >Call Taken</label
+                        >
                       </div>
                     </div>
                   </PopoverPanel>
