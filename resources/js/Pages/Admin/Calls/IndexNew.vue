dbb5e606cba165ef32cf0610d7731988f4c21f79
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-11-23 04:14:31 +0000

    Update

--- resources/js/Pages/Admin/Calls/IndexNew.vue
+++ resources/js/Pages/Admin/Calls/IndexNew.vue
@@ -217,6 +217,7 @@ let getCallerIdColumn = (call) => {
                     :key="colIndex"
                     class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                     v-show="column.visible"
+                    v-text="call[column.columnMethod]"
                   >
                     <!-- Display data based on column method -->
                     {{ call[column.columnMethod](call) }}
