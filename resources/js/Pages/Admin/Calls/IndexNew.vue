b34cbcbb38b2eb667522072bbf86b970e12fbd6e
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-11-23 06:19:43 +0000

    Update

--- resources/js/Pages/Admin/Calls/IndexNew.vue
+++ resources/js/Pages/Admin/Calls/IndexNew.vue
@@ -134,26 +134,6 @@ let callColumnMethod = (call, column) => {
             <div
               class="flex flex-col flex-shrink-0 space-y-3 md:flex-row md:items-center lg:justify-end md:space-y-0 md:space-x-3"
             >
-              <button
-                type="button"
-                class="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 focus:outline-none"
-              >
-                <svg
-                  class="h-3.5 w-3.5 mr-2"
-                  fill="currentColor"
-                  viewbox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true"
-                >
-                  <path
-                    clip-rule="evenodd"
-                    fill-rule="evenodd"
-                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-                  />
-                </svg>
-                Add new product
-              </button>
-
               <div class="px-4">
                 <Popover class="relative">
                   <PopoverButton>
