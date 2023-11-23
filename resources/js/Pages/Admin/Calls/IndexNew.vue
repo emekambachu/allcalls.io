507ae6782a853f2e4ee420504f95f30d051af112
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-11-23 23:11:39 +0000

    Update

--- resources/js/Pages/Admin/Calls/IndexNew.vue
+++ resources/js/Pages/Admin/Calls/IndexNew.vue
@@ -279,47 +279,49 @@ let sortByColumn = (column) => {
                 <tr class="cursor-pointer">
                   <th
                     scope="col"
-                    class="px-4 py-3 whitespace-nowrap flex items-center"
+                    class="px-4 py-3 whitespace-nowrap"
                     v-for="(column, index) in columns"
                     :key="index"
                     v-show="column.visible"
                     @click="sortByColumn(column)"
                   >
-                    <span>{{ column.label }}</span>
+                    <div class="flex items-center">
+                      <span>{{ column.label }}</span>
 
-                    <span v-if="sortColumn === column.columnMethod">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-6 h-6 ml-1"
-                        v-if="sortDirection === 'asc'"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M4.5 15.75l7.5-7.5 7.5 7.5"
-                        />
-                      </svg>
+                      <span v-if="sortColumn === column.columnMethod">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          class="w-6 h-6 ml-1"
+                          v-if="sortDirection === 'asc'"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M4.5 15.75l7.5-7.5 7.5 7.5"
+                          />
+                        </svg>
 
-                      <svg
-                        v-if="sortDirection === 'desc'"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-6 h-6 ml-1"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                        />
-                      </svg>
-                    </span>
+                        <svg
+                          v-if="sortDirection === 'desc'"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          class="w-6 h-6 ml-1"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                          />
+                        </svg>
+                      </span>
+                    </div>
                   </th>
                   <th scope="col" class="px-4 py-3 whitespace-nowrap">Actions</th>
                 </tr>
