6c885732986ef1609a90d8f76514dcada38d2669
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-11-23 00:19:22 +0000

    Update

--- resources/js/Pages/Admin/Calls/IndexNew.vue
+++ resources/js/Pages/Admin/Calls/IndexNew.vue
@@ -100,27 +100,52 @@ let paginate = (url) => {
                 </svg>
                 Update stocks 1/250
               </button>
-              <button
-                type="button"
-                class="flex items-center justify-center flex-shrink-0 px-3 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200"
-              >
-                <svg
-                  class="w-4 h-4 mr-2"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewbox="0 0 24 24"
-                  stroke-width="2"
-                  stroke="currentColor"
-                  aria-hidden="true"
+
+              <Menu as="div" class="relative inline-block text-left">
+                <div>
+                  <MenuButton
+                    class="flex items-center justify-center flex-shrink-0 px-3 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200"
+                  >
+                    <svg
+                      class="w-4 h-4 mr-2"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewbox="0 0 24 24"
+                      stroke-width="2"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+                      />
+                    </svg>
+                    Visible Columns
+                  </MenuButton>
+                </div>
+
+                <transition
+                  enter-active-class="transition duration-100 ease-out"
+                  enter-from-class="transform scale-95 opacity-0"
+                  enter-to-class="transform scale-100 opacity-100"
+                  leave-active-class="transition duration-75 ease-in"
+                  leave-from-class="transform scale-100 opacity-100"
+                  leave-to-class="transform scale-95 opacity-0"
                 >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
-                  />
-                </svg>
-                Export
-              </button>
+                  <MenuItems
+                    class="absolute mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg"
+                  >
+                    <div class="px-1 py-1">
+                      <MenuItem>
+                        <span class="text-gray-900 block px-4 py-2 text-sm">
+                          Hello, world
+                        </span>
+                      </MenuItem>
+                    </div>
+                  </MenuItems>
+                </transition>
+              </Menu>
             </div>
           </div>
           <div v-if="calls.data.length" class="overflow-x-auto">
@@ -131,7 +156,9 @@ let paginate = (url) => {
                   <th scope="col" class="px-4 py-3 whitespace-nowrap">Call Date</th>
                   <th scope="col" class="px-4 py-3 whitespace-nowrap">Agent Name</th>
                   <th scope="col" class="px-4 py-3 whitespace-nowrap">Role</th>
-                  <th scope="col" class="px-4 py-3 whitespace-nowrap">Connected Duration</th>
+                  <th scope="col" class="px-4 py-3 whitespace-nowrap">
+                    Connected Duration
+                  </th>
                   <th scope="col" class="px-4 py-3 whitespace-nowrap">Revenue</th>
                   <th scope="col" class="px-4 py-3 whitespace-nowrap">Vertical</th>
                   <th scope="col" class="px-4 py-3 whitespace-nowrap">Caller ID</th>
