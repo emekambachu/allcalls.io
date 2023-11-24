41c6d1ebb5031b21f00a942d7b6dc4cb897a2744
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-11-24 04:15:18 +0000

    Update

--- resources/js/Pages/Admin/Calls/IndexNew.vue
+++ resources/js/Pages/Admin/Calls/IndexNew.vue
@@ -353,19 +353,6 @@ let filteredCalls = computed(() => {
                           v-text="filter.label"
                         ></label>
                       </div>
-
-                      <div class="flex items-center mb-4">
-                        <input
-                          :id="`filter-2`"
-                          type="checkbox"
-                          class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                        />
-                        <label
-                          :for="`filter-2`"
-                          class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300 select-none"
-                          >Unpaid Calls</label
-                        >
-                      </div>
                     </div>
                   </PopoverPanel>
                 </Popover>
