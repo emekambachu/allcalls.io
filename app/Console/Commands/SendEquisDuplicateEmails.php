af033a20a30d8dae2f8dea71bef402eb39944146
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-11-27 09:18:36 +0000

    Update

---
+++ app/Console/Commands/SendEquisDuplicateEmails.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Mail\EquisDuplicateMail;
+use Illuminate\Support\Facades\Mail;
+
+class SendEquisDuplicateEmails extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'allcalls:send-equis-duplicate-emails';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command to send out daily emails for equis duplicate leads.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Mail::to(['iamfaizahmed123@gmail.com'])
+        ->send(new EquisDuplicateMail('FirstName' . " " . 'LastName', 'EF222171', 'email@example.com'));
+    }
+}
