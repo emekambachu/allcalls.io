64f714ca87e74215e3df0974594472d4f8e39260
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-11-28 10:39:39 +0000

    Update

--- app/Http/Controllers/Admin/CustomerController.php
+++ app/Http/Controllers/Admin/CustomerController.php
@@ -2,26 +2,27 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
-use App\Models\Activity;
+use Exception;
+use App\Models\Bid;
 use App\Models\Call;
-use App\Models\CallType;
 use App\Models\Card;
 use App\Models\Role;
-use App\Models\State;
-use App\Models\UserCallTypeState;
-use Exception;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rule;
-use Inertia\Inertia;
-use App\Models\Transaction;
 use App\Models\User;
-use Illuminate\Validation\Rules\Password;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\DB;
-use App\Models\Bid;
+use Inertia\Inertia;
+use App\Models\State;
 use App\Models\Client;
+use App\Models\Activity;
+use App\Models\CallType;
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use App\Models\NotificationGroup;
+use App\Models\UserCallTypeState;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
 
 class CustomerController extends Controller
 {
@@ -147,6 +148,7 @@ class CustomerController extends Controller
         UserCallTypeState::where('user_id', $user->id)->delete();
         Transaction::where('user_id', $user->id)->delete();
         DB::table('role_user')->where('user_id', $user->id)->delete();
+        NotificationGroup::whereUserId($user->id)->delete();
         $user->delete();
 
         return response()->json([
