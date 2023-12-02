9054d3d18c19fcdd9559d3b7dc70e61b975d969e
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-12-02 15:02:27 +0000

    Add support for filters in WebCallsAPIController

--- app/Http/Controllers/WebCallsAPIController.php
+++ app/Http/Controllers/WebCallsAPIController.php
@@ -8,20 +8,24 @@ use Illuminate\Http\Request;
 class WebCallsAPIController extends Controller
 {
     protected $supportedSortColumns = ['id'];
+    protected $supportedFilters = ['id'];
 
     public function index(Request $request)
     {
         $query = Call::whereUserId($request->user()->id);
 
-        // Retrieve sort column and direction from request
         $sortColumn = $request->input('sort_column', 'id');
         $sortDirection = $request->input('sort_direction', 'desc');
 
-        // Validate sort column
         if (in_array($sortColumn, $this->supportedSortColumns)) {
-            // Validate sort direction
             $sortDirection = $sortDirection === 'asc' ? 'asc' : 'desc';
             $query->orderBy($sortColumn, $sortDirection);
+        }
+
+        foreach ($this->supportedFilters as $filterKey) {
+            if ($request->has($filterKey)) {
+                $query->where($filterKey, $request->input($filterKey));
+            }
         }
 
         $calls = $query->paginate();
