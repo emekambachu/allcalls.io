1875e3a87f943a23d1643b0054ff1720c1cac909
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-12-02 13:09:51 +0000

    Add sorting functionality to WebCallsAPIController index method

--- app/Http/Controllers/WebCallsAPIController.php
+++ app/Http/Controllers/WebCallsAPIController.php
@@ -7,10 +7,26 @@ use Illuminate\Http\Request;
 
 class WebCallsAPIController extends Controller
 {
+    protected $supportedSortColumns = ['id'];
+
     public function index(Request $request)
     {
-        $calls = Call::whereUserId($request->user()->id)->latest()->paginate();
+        $query = Call::whereUserId($request->user()->id);
+
+        // Retrieve sort column and direction from request
+        $sortColumn = $request->input('sort_column', 'id'); // Default to 'id' if not specified
+        $sortDirection = $request->input('sort_direction', 'desc'); // Default to 'desc' if not specified
+
+        // Validate sort column
+        if (in_array($sortColumn, $this->supportedSortColumns)) {
+            // Validate sort direction
+            $sortDirection = $sortDirection === 'asc' ? 'asc' : 'desc'; // Default to 'desc' if invalid value provided
+            $query->orderBy($sortColumn, $sortDirection);
+        }
+
+        $calls = $query->paginate();
 
         return ['calls' => $calls];
     }
+
 }
