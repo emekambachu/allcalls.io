38939da1b8f1b7a94fc44aa96a973c3414c6bf7a
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-12-02 15:16:50 +0000

    Add all columns to supported sort and filter arrays, apply sorting and filtering

--- app/Http/Controllers/WebCallsAPIController.php
+++ app/Http/Controllers/WebCallsAPIController.php
@@ -7,21 +7,31 @@ use Illuminate\Http\Request;
 
 class WebCallsAPIController extends Controller
 {
-    protected $supportedSortColumns = ['id'];
-    protected $supportedFilters = ['id'];
+    // Add all columns to the supported sort and filter arrays
+    protected $supportedSortColumns = [
+        'id', 'user_id', 'call_taken', 'call_duration_in_seconds', 'hung_up_by',
+        'amount_spent', 'recording_url', 'call_type_id', 'created_at', 'updated_at',
+        'sid', 'unique_call_id', 'from', 'user_response_time', 'completed_at'
+    ];
+    protected $supportedFilters = [
+        'id', 'user_id', 'call_taken', 'call_duration_in_seconds', 'hung_up_by',
+        'amount_spent', 'recording_url', 'call_type_id', 'created_at', 'updated_at',
+        'sid', 'unique_call_id', 'from', 'user_response_time', 'completed_at'
+    ];
 
     public function index(Request $request)
     {
         $query = Call::whereUserId($request->user()->id);
 
+        // Apply sorting
         $sortColumn = $request->input('sort_column', 'id');
         $sortDirection = $request->input('sort_direction', 'desc');
-
         if (in_array($sortColumn, $this->supportedSortColumns)) {
             $sortDirection = $sortDirection === 'asc' ? 'asc' : 'desc';
             $query->orderBy($sortColumn, $sortDirection);
         }
 
+        // Apply filtering
         foreach ($this->supportedFilters as $filterKey) {
             if ($request->has($filterKey)) {
                 $query->where($filterKey, $request->input($filterKey));
@@ -32,5 +42,4 @@ class WebCallsAPIController extends Controller
 
         return ['calls' => $calls];
     }
-
 }
