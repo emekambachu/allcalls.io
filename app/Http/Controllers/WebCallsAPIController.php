27d4ff3d37a48a2ef68078d594bbae069a210cac
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-12-04 11:30:10 +0000

    Update

--- app/Http/Controllers/WebCallsAPIController.php
+++ app/Http/Controllers/WebCallsAPIController.php
@@ -22,7 +22,7 @@ class WebCallsAPIController extends Controller
 
     public function index(Request $request)
     {
-        $query = Call::with('user')->with('callType');
+        $query = Call::with('user.role')->with('callType');
 
         // Apply sorting
         $sortColumn = $request->input('sort_column', 'id');
