5b7b723cbc7e99eb23bb2f79127c9e8d8bebeb54
Muneeb Qadoos <iammmuneeb@gmail.com>
2023-12-04 10:36:33 +0000

    Update

--- routes/api.php
+++ routes/api.php
@@ -60,7 +60,7 @@ use App\Http\Controllers\UserRoleController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::middleware('auth:sanctum')->get('user/{id}/roles', [UserRoleController::class, 'getUserRoles']);
+Route::middleware('auth:sanctum')->get('user/roles', [UserRoleController::class, 'getUserRoles']);
 
 
 Route::post('/sanctum/token', function (Request $request) {
