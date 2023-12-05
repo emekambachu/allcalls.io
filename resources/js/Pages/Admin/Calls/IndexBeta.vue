42636c774277c59169b5521f01cb850fa08c1ccb
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-12-05 20:32:26 +0000

    Add cursor pointer to export CSV icon

--- resources/js/Pages/Admin/Calls/IndexBeta.vue
+++ resources/js/Pages/Admin/Calls/IndexBeta.vue
@@ -555,7 +555,7 @@ let exportCSV = () => {
                       viewBox="0 0 24 24"
                       stroke-width="1.5"
                       stroke="currentColor"
-                      class="w-3 h-3"
+                      class="w-3 h-3 cursor-pointer"
                     >
                       <path
                         stroke-linecap="round"
