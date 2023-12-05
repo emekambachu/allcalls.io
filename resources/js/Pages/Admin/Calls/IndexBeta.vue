9c6df8c5269735b54b2a6e7acccbeeb1e6ed32cc
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-12-05 21:07:41 +0000

    Update

--- resources/js/Pages/Admin/Calls/IndexBeta.vue
+++ resources/js/Pages/Admin/Calls/IndexBeta.vue
@@ -582,24 +582,35 @@ let stopPlayingRecording = (call) => {
                       />
                     </svg>
 
-                    <svg
-                      class="w-4 h-4 cursor-pointer ml-3"
-                      v-if="playingRecordingCallIds.includes(call.id) && currentlyPlayingAudio !== null"
-                      @click.prevent="stopPlayingRecording(call)"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z"
-                      />
-                    </svg>
+                    <div class="flex">
+                      <svg
+                        class="w-4 h-4 cursor-pointer ml-3"
+                        v-if="
+                          playingRecordingCallIds.includes(call.id) &&
+                          currentlyPlayingAudio !== null
+                        "
+                        @click.prevent="stopPlayingRecording(call)"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z"
+                        />
+                      </svg>
 
-                    <p style="font-size: 10px;" class="text-gray-800 ml-1" v-if="currentlyPlayingAudio !== null">Playing</p>
+                      <p
+                        style="font-size: 10px"
+                        class="text-gray-800 ml-1"
+                        v-if="currentlyPlayingAudio !== null"
+                      >
+                        Playing
+                      </p>
+                    </div>
                   </td>
                   <td class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap">
                     <!-- Actions column content -->
