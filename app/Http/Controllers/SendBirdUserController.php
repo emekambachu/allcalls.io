b7271eeca1d25c5c2202b809a8a9fe307872c199
Muneeb Qadoos <iammmuneeb@gmail.com>
2023-12-05 23:11:42 +0000

    Update

--- app/Http/Controllers/SendBirdUserController.php
+++ app/Http/Controllers/SendBirdUserController.php
@@ -14,87 +14,89 @@ class SendBirdUserController extends Controller
     public function createSendBirdUser(Request $request)
     {
         $user = Auth::user(); // Get the authenticated user
-        Log::debug('Creating SendBird user for user ID: ' . $user->id);
+        // Log::debug('Creating SendBird user for user ID: ' . $user->id);
+        return response()->json([
+                'message' => 'SendBird user created successfully'
+            ], 200);
+        // // Check if the user has the required roles
+        // if (!$user->roles->contains('name', 'internal-agent') && !$user->roles->contains('name', 'admin')) {
+        //     Log::warning('User does not have required roles. User ID: ' . $user->id);
+        //     return response()->json(['message' => 'User is not an internal agent or admin'], 403);
+        // }
 
-        // Check if the user has the required roles
-        if (!$user->roles->contains('name', 'internal-agent') && !$user->roles->contains('name', 'admin')) {
-            Log::warning('User does not have required roles. User ID: ' . $user->id);
-            return response()->json(['message' => 'User is not an internal agent or admin'], 403);
-        }
+        // // Check if a SendBird user already exists for the user
+        // $existingSendBirdUser = $user->sendBirdUser()->first();
+        // if ($existingSendBirdUser) {
+        //     Log::info('SendBird user already exists for user ID: ' . $user->id);
+        //     return response()->json(['message' => 'SendBird user already exists'], 409);
+        // }
 
-        // Check if a SendBird user already exists for the user
-        $existingSendBirdUser = $user->sendBirdUser()->first();
-        if ($existingSendBirdUser) {
-            Log::info('SendBird user already exists for user ID: ' . $user->id);
-            return response()->json(['message' => 'SendBird user already exists'], 409);
-        }
+        // // Validate incoming request fields
+        // $validatedData = $request->validate([
+        //     'nickname' => 'required|string|max:255',
+        //     // 'profile_image' => 'required|image', // Validate the image
+        // ]);
 
-        // Validate incoming request fields
-        $validatedData = $request->validate([
-            'nickname' => 'required|string|max:255',
-            // 'profile_image' => 'required|image', // Validate the image
-        ]);
-
-        // Handle Image Upload
-        $path = $request->file('profile_image')->storeAs(
-            'profile_pictures', $user->id . '.' . $request->file('profile_image')->extension()
-        );
-        $profileUrl = Storage::url($path);
-        $fullUrl = url($profileUrl);
+        // // Handle Image Upload
+        // $path = $request->file('profile_image')->storeAs(
+        //     'profile_pictures', $user->id . '.' . $request->file('profile_image')->extension()
+        // );
+        // $profileUrl = Storage::url($path);
+        // $fullUrl = url($profileUrl);
         
-        // Update user's profile picture in the database
-        $user->profile_picture = $profileUrl;
-        $user->save();
-        Log::debug('Profile image uploaded. Path: ' . $path);
+        // // Update user's profile picture in the database
+        // $user->profile_picture = $profileUrl;
+        // $user->save();
+        // Log::debug('Profile image uploaded. Path: ' . $path);
 
-        $applicationId = env('SENDBIRD_APPLICATION_ID'); // Replace with your SendBird application ID
-        $apiKey = env('SENDBIRD_API_TOKEN'); // Replace with your SendBird API key
+        // $applicationId = env('SENDBIRD_APPLICATION_ID'); // Replace with your SendBird application ID
+        // $apiKey = env('SENDBIRD_API_TOKEN'); // Replace with your SendBird API key
 
-        $response = Http::withHeaders([
-            'Api-Token' => $apiKey,
-            'Content-Type' => 'application/json',
-        ])->post("https://api-{$applicationId}.sendbird.com/v3/users", [
-            'user_id' => $user->id,
-            'nickname' => $request->nickname,
-            'profile_url' => $fullUrl,
-            'issue_access_token' => true, // Include this field
-        ]);
+        // $response = Http::withHeaders([
+        //     'Api-Token' => $apiKey,
+        //     'Content-Type' => 'application/json',
+        // ])->post("https://api-{$applicationId}.sendbird.com/v3/users", [
+        //     'user_id' => $user->id,
+        //     'nickname' => $request->nickname,
+        //     'profile_url' => $fullUrl,
+        //     'issue_access_token' => true, // Include this field
+        // ]);
 
-        if ($response->successful()) {
-            $responseData = $response->json();
+        // if ($response->successful()) {
+        //     $responseData = $response->json();
         
-            $sendBirdUser = new SendBirdUser([
-                'nickname' => $responseData['nickname'],
-                'user_id' => $user->id,
-                'profile_url' => $responseData['profile_url'],
-                'access_token' => $responseData['access_token'],
-                'is_online' => $responseData['is_online'],
-                'is_active' => $responseData['is_active'],
-                'is_created' => $responseData['is_created'],
-                'phone_number' => $responseData['phone_number'],
-                'require_auth_for_profile_image' => $responseData['require_auth_for_profile_image'],
-                'session_tokens' => json_encode($responseData['session_tokens']),
-                'last_seen_at' => $responseData['last_seen_at'],
-                'discovery_keys' => json_encode($responseData['discovery_keys']),
-                'preferred_languages' => json_encode($responseData['preferred_languages']),
-                'has_ever_logged_in' => $responseData['has_ever_logged_in'],
-            ]);
+        //     $sendBirdUser = new SendBirdUser([
+        //         'nickname' => $responseData['nickname'],
+        //         'user_id' => $user->id,
+        //         'profile_url' => $responseData['profile_url'],
+        //         'access_token' => $responseData['access_token'],
+        //         'is_online' => $responseData['is_online'],
+        //         'is_active' => $responseData['is_active'],
+        //         'is_created' => $responseData['is_created'],
+        //         'phone_number' => $responseData['phone_number'],
+        //         'require_auth_for_profile_image' => $responseData['require_auth_for_profile_image'],
+        //         'session_tokens' => json_encode($responseData['session_tokens']),
+        //         'last_seen_at' => $responseData['last_seen_at'],
+        //         'discovery_keys' => json_encode($responseData['discovery_keys']),
+        //         'preferred_languages' => json_encode($responseData['preferred_languages']),
+        //         'has_ever_logged_in' => $responseData['has_ever_logged_in'],
+        //     ]);
         
-            $sendBirdUser->save();
-            Log::info('SendBird user created successfully for user ID: ' . $user->id);
+        //     $sendBirdUser->save();
+        //     Log::info('SendBird user created successfully for user ID: ' . $user->id);
 
-            return response()->json([
-                'message' => 'SendBird user created successfully',
-                'sendBirdUser' => $sendBirdUser
-            ], 201);
-        } else {
-            // Handle failure response from SendBird API
-            Log::error('Failed to create SendBird user. User ID: ' . $user->id . ' Response: ' . $response->body());
-            return response()->json([
-                'message' => 'Failed to create SendBird user',
-                'error' => $response->body()
-            ], 500);
-        }        
+        //     return response()->json([
+        //         'message' => 'SendBird user created successfully',
+        //         'sendBirdUser' => $sendBirdUser
+        //     ], 201);
+        // } else {
+        //     // Handle failure response from SendBird API
+        //     Log::error('Failed to create SendBird user. User ID: ' . $user->id . ' Response: ' . $response->body());
+        //     return response()->json([
+        //         'message' => 'Failed to create SendBird user',
+        //         'error' => $response->body()
+        //     ], 500);
+        // }        
     }
 
     public function checkSendBirdUser(Request $request)
