a6b90a432f6d8ce0e8b0ec1f76f38f3fa56839d0
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-12-05 20:55:38 +0000

    Update

--- resources/js/Pages/Admin/Calls/IndexBeta.vue
+++ resources/js/Pages/Admin/Calls/IndexBeta.vue
@@ -151,7 +151,7 @@ let fetchCalls = async (replace = false) => {
 
   callsPaginator.value = response.data.calls;
   loading.value = false;
-  console.log('Loaded Calls: ', loadedCalls.value);
+  console.log("Loaded Calls: ", loadedCalls.value);
 };
 
 let loadMore = async () => {
@@ -262,10 +262,22 @@ let exportCSV = () => {
   document.body.removeChild(link);
 };
 
-let playRecording = (url) => {
-  let audio = new Audio(url);
-  audio.play();
-}
+let playingRecordingCallIds = ref([]);
+let currentlyPlayingAudio = ref(null);
+
+let playRecording = (call) => {
+  playingRecordingCallIds.value.push(call.id);
+  currentlyPlayingAudio.value = new Audio(call.recording_url);
+  currentlyPlayingAudio.value.play();
+};
+
+let stopPlayingRecording = (call) => {
+  playingRecordingCallIds.value = playingRecordingCallIds.value.filter(
+    (id) => id !== call.id
+  );
+  currentlyPlayingAudio.value.pause();
+  currentlyPlayingAudio.value = null;
+};
 </script>
 
 <template>
@@ -562,12 +574,28 @@ let playRecording = (url) => {
                       stroke-width="1.5"
                       stroke="currentColor"
                       class="w-4 h-4 cursor-pointer ml-3"
-                      @click.prevent="playRecording(call.recording_url)"
+                      @click.prevent="playRecording(call)"
                     >
                       <path
                         stroke-linecap="round"
                         stroke-linejoin="round"
                         d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+                      />
+                    </svg>
+
+                    <svg
+                      class="w-4 h-4 cursor-pointer ml-3"
+                      @click.prevent="stopPlayingRecording(call)"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z"
                       />
                     </svg>
                   </td>
