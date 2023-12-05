449aadaa4f61b3a139663896920c086854ff17cc
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-12-05 20:59:12 +0000

    Update

--- resources/js/Pages/Admin/Calls/IndexBeta.vue
+++ resources/js/Pages/Admin/Calls/IndexBeta.vue
@@ -600,6 +600,8 @@ let stopPlayingRecording = (call) => {
                         d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z"
                       />
                     </svg>
+
+                    <p class="text-xs">Currently Playing</p>
                   </td>
                   <td class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap">
                     <!-- Actions column content -->
