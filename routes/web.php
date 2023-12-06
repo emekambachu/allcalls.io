1a05b3f42d09582b3a7e955474a5321d29878830
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-12-06 21:12:31 +0000

    Update profile information form to use a POST request

--- routes/web.php
+++ routes/web.php
@@ -113,7 +113,7 @@ Route::middleware(['auth', 'verified', 'registration-step-check', 'notBanned'])-
 Route::middleware(['auth', 'notBanned'])->group(function () {
     Route::get('/profile/view', [ProfileController::class, 'view'])->name('profile.view');
     Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::post('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::post('/profile/upload-picture', [ProfileController::class, 'uploadProfilePicture']);
 });
