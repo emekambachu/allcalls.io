5da48e672bbec2950c7dd8abc535f854a31649ae
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-12-09 14:40:20 +0000

    Update TextInput border style

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -23,7 +23,7 @@ defineExpose({ focus: () => input.value.focus() });
 
 <template>
   <input
-    class="bg-custom-blue text-sm rounded-lg focus:ring-blue-500 focus:border focus:border-blue-500 block w-full p-2.5 text-black outline-none"
+    class="bg-custom-blue text-sm rounded-lg focus:ring-blue-500 border border-transparent focus:border focus:border-blue-500 block w-full p-2.5 text-black outline-none"
     style="background-color: #E8F0FE;"
     :value="modelValue"
     @input="$emit('update:modelValue', $event.target.value)"
