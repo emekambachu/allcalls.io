cd1595a5d49b01351914db22c1a10fb88b9f15e6
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-12-09 15:15:29 +0000

    Add filter logic to index method in WebCallsAPIController

--- app/Http/Controllers/WebCallsAPIController.php
+++ app/Http/Controllers/WebCallsAPIController.php
@@ -25,9 +25,21 @@ class WebCallsAPIController extends Controller
             $sortDirection = $sortDirection === 'asc' ? 'asc' : 'desc';
             $query->orderBy($sortColumn, $sortDirection);
         }
-
+    
+        // Retrieve filters
+        $filters = $request->input('filters', []);
+        foreach ($filters as $filter) {
+            if(isset($filter['name'], $filter['value'], $filter['operator'])) {
+                // Apply filter logic here
+                // Example for a simple where clause
+                // Check if the column exists in your model or handle it as needed
+                $query->where($filter['name'], $filter['operator'], $filter['value']);
+            }
+        }
+    
+        // Get paginated result
         $calls = $query->paginate();
-
+    
         return ['calls' => $calls];
     }
 }
