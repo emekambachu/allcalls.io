8456255512e38b508cec2d5e23103a21dc2246d3
awaisamir123 <awaisamir23@gmail.com>
2023-12-09 20:16:02 +0500

    validation issue fix

--- resources/js/Pages/InternalAgent/MyBusiness/ViewDetail.vue
+++ resources/js/Pages/InternalAgent/MyBusiness/ViewDetail.vue
@@ -16,7 +16,6 @@ let close = () => {
 </script>
 <template>
   <!-- This is the overlay -->
-  <AuthenticatedLayout>
     <Transition name="modal" enter-active-class="transition ease-out  duration-300 transform"
       enter-from-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
       enter-to-class="opacity-100 translate-y-0 sm:scale-100"
@@ -49,5 +48,4 @@ let close = () => {
         </div>
       </div>
     </Transition>
-  </AuthenticatedLayout>
 </template>
