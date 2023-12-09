41e53afcb65b3fb002e0ecb603d07bd53ba8286d
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-12-09 15:20:17 +0000

    Update filter modal layout spacing

--- resources/js/Pages/Admin/Calls/IndexBeta.vue
+++ resources/js/Pages/Admin/Calls/IndexBeta.vue
@@ -525,7 +525,7 @@ let applyFilter = () => {
       :closeable="true"
     >
       <div class="bg-gray-100 py-4 px-6 text-gray-900">
-        <div class="flex justify-between mb-4">
+        <div class="flex justify-between mb-6">
           <h3 class="text-2xl font-bold">Add New Filter</h3>
           <span class="cursor-pointer" @click.prevent="showNewFilterModal = false">&#x2715</span>
         </div>
