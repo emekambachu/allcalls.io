b38ef0da9e54547ddf51350f021c049022cf68d6
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-12-11 15:50:53 +0000

    Update

--- resources/js/Pages/InternalAgent/MyBusiness/AddModal.vue
+++ resources/js/Pages/InternalAgent/MyBusiness/AddModal.vue
@@ -2278,7 +2278,7 @@ let selectagent = (agent) => {
                           :disabled="form.this_app_from_lead == 'NO'" id="countries"
                           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                           <option disabled>Select</option>
-                          <option value="Allcalls Client">Allcalls Client</option>
+                          <option value="Allcalls Client">Allcalls</option>
                           <option value="Outside Lead ">Outside Lead </option>
                         </select>
                         <div v-if="firstStepErrors.source_of_lead" class="text-red-500"
