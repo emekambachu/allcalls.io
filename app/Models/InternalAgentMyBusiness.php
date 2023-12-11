4e01a21d2d4b4fdcbd084e16086e7b4aa77c24fb
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-12-11 16:28:21 +0000

    Add getApplicationDateAttribute method to InternalAgentMyBusiness model

--- app/Models/InternalAgentMyBusiness.php
+++ app/Models/InternalAgentMyBusiness.php
@@ -2,11 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class InternalAgentMyBusiness extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+
+    public function getApplicationDateAttribute($value)
+    {
+        $date = Carbon::parse($value);
+    
+        if (auth()->user()) {
+            $timezone = auth()->user()->timezone;
+            $date->timezone($timezone);
+        }
+    
+        return $date->format('m/d/Y');
+    }
+    
+
 }
