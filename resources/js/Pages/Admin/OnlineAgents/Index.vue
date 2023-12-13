854eef00fee2d760ff790df633490099f27b9942
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2023-12-13 13:19:05 +0000

    Update online agents popover panel to allow flex wrapping

--- resources/js/Pages/Admin/OnlineAgents/Index.vue
+++ resources/js/Pages/Admin/OnlineAgents/Index.vue
@@ -235,7 +235,7 @@ let minimizedArray = (arr) => {
                         </PopoverButton>
 
                         <PopoverPanel class="absolute z-10 w-40 -left-20">
-                          <div class="bg-white p-2 border border-gray-100 shadow flex justify-center items-center">
+                          <div class="bg-white p-2 border border-gray-100 shadow flex justify-center items-center flex-wrap">
                             <div
                               v-for="(state, index) in onlineUser.user.states"
                               :key="state.id"
