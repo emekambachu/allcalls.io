ed5f64f731a26b7ecf6bf7dfef2481c468f66e6f
Muneeb Qadoos <muneeb@muneebs-mbp.home>
2024-01-02 03:17:55 +0000

    Update

--- app/Notifications/ClientDispositionReminder.php
+++ app/Notifications/ClientDispositionReminder.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use SebastianBergmann\Type\VoidType;
 
 class ClientDispositionReminder extends Notification
 {
@@ -34,10 +35,10 @@ class ClientDispositionReminder extends Notification
      */
     public function toMail(object $notifiable): MailMessage
     {
-        // return (new MailMessage)
-        //             ->line('The introduction to the notification.')
-        //             ->action('Notification Action', url('/'))
-        //             ->line('Thank you for using our application!');
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
     }
 
     public function toArray(object $notifiable)
