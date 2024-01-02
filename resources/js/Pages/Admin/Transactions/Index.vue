6c095b00157a2e68bdc1dc285646a63c981af6c6
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-01-02 13:04:38 +0000

    Add input field for transaction amount

--- resources/js/Pages/Admin/Transactions/Index.vue
+++ resources/js/Pages/Admin/Transactions/Index.vue
@@ -295,6 +295,9 @@ input[type="number"] {
                   </li>
                 </ul>
 
+                <div class="">
+                  <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+                </div>
                 
               </div>
             </PopoverPanel>
