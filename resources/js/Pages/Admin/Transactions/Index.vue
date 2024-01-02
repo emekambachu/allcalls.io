2b0f48aef5b741db82008b08538c284d07b3b9f5
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-01-02 12:23:42 +0000

    Update PopoverButton class to include flex and full width

--- resources/js/Pages/Admin/Transactions/Index.vue
+++ resources/js/Pages/Admin/Transactions/Index.vue
@@ -243,7 +243,7 @@ input[type="number"] {
           </select> -->
 
           <Popover class="relative mr-2 block w-full">
-            <PopoverButton>
+            <PopoverButton class="flex h-full w-full">
               <button
                 type="button"
                 class="block w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
