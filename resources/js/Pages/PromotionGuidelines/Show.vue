fce3994fa47268ac066dfe553375c4744ed14fcc
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-01-03 15:29:24 +0000

    Refactor PromotionGuidelines/Show.vue and add logic to display different images based on the showChart prop

--- resources/js/Pages/PromotionGuidelines/Show.vue
+++ resources/js/Pages/PromotionGuidelines/Show.vue
@@ -2,29 +2,33 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head } from "@inertiajs/vue3";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
+import { ref } from "vue";
 
-const props = defineProps({});
+let props = defineProps(['showChart'])
 
-let openLiveChat = () => {
-  let iframe = document.querySelector(".widget-visible iframe");
-  let iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-  let iframeButton = iframeDoc.querySelector("button");
+let guidelinesImg = ref(null);
 
-  iframeButton.click();
-};
+onMounted(() => {
+    if (props.showChart === 'small') {
+        guidelinesImg.value = '/img/promotion-guidelines/Internal 1.png';
+    } else if (props.showChart === 'large') {
+        guidelinesImg.value = '/img/promotion-guidelines/External 1.png';
+    }
+}
+
+
+})
 </script>
 
 <template>
   <Head title="Promotion Guidelines" />
   <AuthenticatedLayout>
     <template #header>
-      <h2 class="font-small text-xl text-gray-800 leading-tight">
-        Promotion Guidelines
-      </h2>
+      <h2 class="font-small text-xl text-gray-800 leading-tight">Promotion Guidelines</h2>
     </template>
 
-    <div>
-        <img style="max-width: 100%; transform: scale(1.01);" src="/img/promotion-guidelines/External 1.png">
+    <div v-if="guidelinesImg">
+      <img style="max-width: 100%; transform: scale(1.01)" :src="guidelinesImg" />
     </div>
   </AuthenticatedLayout>
 </template>
