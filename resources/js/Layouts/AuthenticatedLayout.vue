1d1483ea8ac8390630194319e7f0f8260f783d8e
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-01-03 16:10:50 +0000

    Remove unnecessary class attribute from SVG element

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -2112,7 +2112,6 @@ let appDownloadModal = ref(false);
                 viewBox="0 0 24 24"
                 stroke-width="1.5"
                 stroke="currentColor"
-                class="w-6 h-6"
               >
                 <path
                   stroke-linecap="round"
