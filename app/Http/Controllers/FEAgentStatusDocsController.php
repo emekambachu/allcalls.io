41cecb18a8ddcd3c78de8a44b0ff60bcf82474a0
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-01-05 15:36:07 +0000

    Add new FEAgentPingDocsController, FEAgentStatusDocsController, and FEAgentStatusPriceDocsController. Update web routes for new documentation endpoints."Add FEAgentPingDocs and related controllers"

---
+++ app/Http/Controllers/FEAgentStatusDocsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use Illuminate\Http\Request;
+
+class FEAgentStatusDocsController extends Controller
+{
+    public function show()
+    {
+        return Inertia::render('Docs/Ping/Show', [
+            'domain' => 'https://feagentping.com'
+        ]);
+    }
+}
