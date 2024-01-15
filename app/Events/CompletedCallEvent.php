da73e2e60a8018687e6601b79d17079bb8aa9caa
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-01-15 01:18:28 +0000

    Add ShouldBroadcast interface to CompletedCallEvent

--- app/Events/CompletedCallEvent.php
+++ app/Events/CompletedCallEvent.php
@@ -12,7 +12,7 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class CompletedCallEvent
+class CompletedCallEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
