5b0df53011e572ab4f35fba58ab7c019591d27ee
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-01-22 02:14:57 +0000

    Add email to blacklist if not already blacklisted

--- app/Http/Controllers/EmailBlacklistController.php
+++ app/Http/Controllers/EmailBlacklistController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\EmailBlacklist;
+use DocuSign\eSign\Model\Email;
 
 class EmailBlacklistController extends Controller
 {
     public function store(Request $request)
     {
+        $exists = EmailBlacklist::where('email', $request->user()->email)->exists();
+
+        if ($exists) {
+            return redirect()->route('take-calls.show')->with(['message' => 'You have already unsubscribed from our mailing list.']);
+        }
+
         EmailBlacklist::create([
             'email' => $request->user()->email,
         ]);
