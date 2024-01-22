6f588119284d0db5b666929475a287b2e5632352
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-01-22 03:29:14 +0000

    Add Unsubscribe Token to User model and create UnsubscribeToken model and migration

---
+++ app/Listeners/AddUnsubscribeTokenToUser.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Models\User;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class AddUnsubscribeTokenToUser
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(object $event)
+    {
+        // Check if 'user' and 'email' keys are available in the event data
+        if (!isset($event->data['user']['email'])) {
+            return true;
+        }
+
+        $email = $event->data['user']['email'];
+
+
+        $user = User::where('email', $email)->first();
+
+        if ($user && !$user->unsubscribeToken()) {
+            $user->generateUnsubscribeToken();
+        }
+    }
+}
