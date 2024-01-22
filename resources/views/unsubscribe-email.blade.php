e9b55cdffbd84e953ca90f8b2672623fc520e969
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-01-22 04:56:56 +0000

    Update flash message margin in unsubscribe email view

--- resources/views/unsubscribe-email.blade.php
+++ resources/views/unsubscribe-email.blade.php
@@ -15,12 +15,12 @@
 
             <!-- Flash Messages -->
             @if (session('message'))
-                <div class="p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800" role="alert">
+                <div class="p-4 my-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800" role="alert">
                     {{ session('message') }}
                 </div>
             @endif
             @if (session('error'))
-                <div class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
+                <div class="p-4 my-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
                     {{ session('error') }}
                 </div>
             @endif
