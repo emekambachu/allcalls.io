1839351549e69f364f4f6d962a2a10c9acbc803f
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-01-22 02:02:07 +0000

    Add unsubscribe link to email footer

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -11,17 +11,20 @@
 
     {{-- Subcopy --}}
     @isset($subcopy)
-        <x-slot:subcopy>
-            <x-mail::subcopy>
-                {{ $subcopy }}
-            </x-mail::subcopy>
-        </x-slot:subcopy>
+    <x-slot:subcopy>
+        <x-mail::subcopy>
+            {{ $subcopy }}
+        </x-mail::subcopy>
+    </x-slot:subcopy>
     @endisset
 
     {{-- Footer --}}
     <x-slot:footer>
         <x-mail::footer>
             © {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
+            <br>
+            <a href="#" style="color: #999999; text-decoration: underline;">Unsubscribe</a>
+
         </x-mail::footer>
     </x-slot:footer>
 </x-mail::layout>
