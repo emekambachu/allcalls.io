226dbb7a43fc91777b09f1c1a57f4ee559b2e17f
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-01-25 08:42:53 +0000

    Disable low balance modal in AuthenticatedLayout.vue

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -24,7 +24,7 @@ console.log(page.props.auth.user.balance);
 let showLowBalanceModal = ref(false);
 if (page.props.auth.role !== 'admin' && page.props.auth.user.balance < 35) {
   console.log('Display the low balance modal now.');
-  showLowBalanceModal.value = true;
+  // showLowBalanceModal.value = true;
 }
 
 let onLowBalanceModalClick = () => {
