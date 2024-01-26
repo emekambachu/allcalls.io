c6dd1248120a9f051ea9a7e38af7ed1d7c300b93
awaisamir123 <awaisamir23@gmail.com>
2024-01-27 01:49:51 +0500

    default report application status submitted

--- app/Http/Controllers/Admin/AgentBusinessController.php
+++ app/Http/Controllers/Admin/AgentBusinessController.php
@@ -245,8 +245,8 @@ class AgentBusinessController extends Controller
 
     public function testReportApplication($id) {
         $business = InternalAgentMyBusiness::findOrFail($id);
-        $business->application_date = Carbon::now()->addDay();
-        $business->policy_draft_date = Carbon::now()->addWeek();
+        $business->application_date = Carbon::now()->format('Y-d-m');
+        $business->policy_draft_date = Carbon::now()->addDay('Y-d-m');
         $business->save();
         dd($business);
     }
