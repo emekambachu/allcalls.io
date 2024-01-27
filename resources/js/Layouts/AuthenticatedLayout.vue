746eb35ffd311a654b6986365d332e3d44bb95d9
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-01-27 20:48:05 +0000

    Update call disposition modal with client details

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -3005,11 +3005,34 @@ let appDownloadModal = ref(false);
 
     <Modal :show="showUpdateDispositionForLastClient" :closeable="false">
       <div class="bg-white p-6 rounded-lg shadow">
-        <h2 class="text-xl font-semibold text-gray-900 mb-4">Update Call Disposition</h2>
-        <p class="text-gray-700 mb-6">
-          You've just completed a call. Please update the client's disposition to ensure
-          accurate tracking and follow-up actions.
-        </p>
+        <div
+          class="mb-4 flex rounded-lg bg-blue-50 p-4 text-sm text-blue-800"
+          role="alert"
+        >
+          <svg
+            class="me-3 mt-[2px] inline h-4 w-4 flex-shrink-0"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"
+            />
+          </svg>
+          <span class="sr-only">Info</span>
+          <div>
+            <span class="font-medium">Attention Required:</span>
+            <p class="mb-4 mt-1.5">
+              You've just completed a call. Please update the client's disposition to
+              ensure accurate tracking and follow-up actions:
+            </p>
+            <ul class="list-inside list-disc">
+              <li><span class="font-bold">First Name:</span> John</li>
+              <li><span class="font-bold">Last Name:</span> Doe</li>
+            </ul>
+          </div>
+        </div>
 
         <div class="mb-6">
           <label
