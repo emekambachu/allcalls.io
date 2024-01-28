e2701b645f0a9df0aa62c9e43ea6fcc77c983409
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-01-28 04:10:25 +0000

    Update CSS class for DisclosureButton in ClientPolicies.vue

--- resources/js/Components/ClientPolicies.vue
+++ resources/js/Components/ClientPolicies.vue
@@ -20,7 +20,7 @@ let policies = reactive([
 <template>
   <div>
     <Disclosure v-slot="{ open }">
-      <DisclosureButton class="flex text-xl font-semibold text-gray-900">
+      <DisclosureButton class="flex items-center text-xl font-semibold text-gray-900">
         <span class="mr-2"> Manage Attached Policies </span>
         <span v-if="!open">
           <svg
