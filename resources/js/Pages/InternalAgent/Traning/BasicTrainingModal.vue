6fc72cc7c80ce16db0d16dbcb69e1e1516346d18
awaisamir123 <awaisamir23@gmail.com>
2024-02-05 22:20:09 +0500

    training page

--- resources/js/Pages/InternalAgent/Traning/BasicTrainingModal.vue
+++ resources/js/Pages/InternalAgent/Traning/BasicTrainingModal.vue
@@ -103,7 +103,7 @@ let downloadPDF = async (pdfPath) => {
                   :href="videoData.pdf"
                   class="px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
                 >
-                Download Finish Contracting Pdf
+                Download {{videoData.title  }} Pdf
                 </a>
                 <!-- <a target="_blank" :href="videoData.pdf" 
                                     class="font-medium  text-blue-600  cursor-pointer dark:text-blue-500 hover:underline">Download
