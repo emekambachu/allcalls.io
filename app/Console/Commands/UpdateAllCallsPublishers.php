b1625e9c099a6aa4aaf9fecf88c715e9aca5bdfc
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-02-06 13:02:00 +0000

    Update publisher info for calls without info

--- app/Console/Commands/UpdateAllCallsPublishers.php
+++ app/Console/Commands/UpdateAllCallsPublishers.php
@@ -2,7 +2,6 @@
 
 namespace App\Console\Commands;
 
-use Carbon\Carbon;
 use App\Models\Call;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
@@ -23,25 +22,19 @@ class UpdateAllCallsPublishers extends Command
      */
     protected $description = 'Update the publisher info in the calls existing call records.';
 
+    /**
+     * Execute the console command.
+     */
     public function handle()
     {
-        $query = Call::whereNull('publisher_name')->whereNull('publisher_id');
-
-        // Check if the --only-last-week option is provided:
-        if ($this->option('only-last-week')) {
-            $weekAgo = Carbon::now()->subDays(7);
-            $query->where('created_at', '>=', $weekAgo);
-        }
-
-        $calls = $query->get();
+        // Get all calls where publisher_name and publisher_id is null:
+        $calls = Call::whereNull('publisher_name')->whereNull('publisher_id')->get();
 
         $this->info('Found ' . $calls->count() . ' calls without publisher info.');
 
         // Loop through each call and update the publisher info:
         foreach ($calls as $call) {
             $response = $call->updatePublisherInfo();
-
-            Log::debug('updatePublisherInfo:', ['response' => $response, 'call_id' => $call->id]);
 
             if ($response) {
                 $this->info('Updated call ' . $call->id . ' with publisher info.');
