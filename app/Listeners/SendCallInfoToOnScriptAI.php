864099f4f3a05a5834832054995d49b2ab4a8906
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-02-06 17:51:32 +0000

    Add client information to the call payload

--- app/Listeners/SendCallInfoToOnScriptAI.php
+++ app/Listeners/SendCallInfoToOnScriptAI.php
@@ -36,13 +36,17 @@ class SendCallInfoToOnScriptAI implements ShouldQueue
                 'agent_name' => $agent->first_name . ' ' . $agent->last_name,
                 'api_key' => env('ONSCRIPT_AI_API_KEY'),
                 'url' => $call->recording_url ?? 'N/A',
-                'client_phone' => $call->from,
                 'first_name' => $agent->first_name,
                 'last_name' => $agent->last_name,
                 'call_timestamp' => $call->created_at->format('Y-m-d H:i:s'),
                 'affiliate_name' => $call->publisher_name ?? null,
                 'call_disposition' => ($call->client && $call->client->status) ? $call->client->status : null,
                 'agent_id' => $agent->id, // Added parameter
+                'first_name' => ($call->client && $call->client->first_name) ? $call->client->first_name: null,
+                'last_name' => ($call->client && $call->client->last_name) ? $call->client->last_name: null,
+                'client_phone' => $call->from,
+                'client_address' => ($call->client && $call->client->address) ? $call->client->address : null,
+                'client_zipcode' => ($call->client && $call->client->zipCode) ? $call->client->zipCode : null,
             ];
 
             // Filter out null values
