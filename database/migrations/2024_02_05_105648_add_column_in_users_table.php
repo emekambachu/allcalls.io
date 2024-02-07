1b036df1077db21832e490c89f8f02c2bd57a8bf
awaisamir123 <awaisamir23@gmail.com>
2024-02-07 21:52:42 +0500

    inviter upline_id store as a manager_id of invitee

--- database/migrations/2024_02_05_105648_add_column_in_users_table.php
+++ database/migrations/2024_02_05_105648_add_column_in_users_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
+
             $table->string('ef_number')->nullable()->after('upline_id');
+
         });
     }
 
