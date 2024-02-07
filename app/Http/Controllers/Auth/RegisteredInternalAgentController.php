f20de60cedc18459c01c2cda584839c38a7debc7
awaisamir123 <awaisamir23@gmail.com>
2024-02-07 16:47:14 +0500

    test for agent ef_nnumber

--- app/Http/Controllers/Auth/RegisteredInternalAgentController.php
+++ app/Http/Controllers/Auth/RegisteredInternalAgentController.php
@@ -140,7 +140,7 @@ class RegisteredInternalAgentController extends Controller
             "languageId" => "en",
             "lastName" => $user->internalAgentContract->last_name ?? null,
             "npn" => $user->internalAgentContract->resident_insu_license_no ?? null,
-            "partnerUniqueId" => "AC" . $user->id,
+            "partnerUniqueId" => "AC" . '1201',
             "role" => "Agent",
             "details" => "This is test agent",
             "state" => isset($user->internalAgentContract->state) ? $user->internalAgentContract->getState->name : null,
