7ba388e8fe3524591fd97db5044f4e888c06e9b5
awaisamir123 <awaisamir23@gmail.com>
2024-02-07 18:50:09 +0500

    test for agent ef_nnumber

--- app/Http/Controllers/Auth/RegisteredInternalAgentController.php
+++ app/Http/Controllers/Auth/RegisteredInternalAgentController.php
@@ -140,11 +140,11 @@ class RegisteredInternalAgentController extends Controller
             "languageId" => "en",
             "lastName" => $user->internalAgentContract->last_name ?? null,
             "npn" => $user->internalAgentContract->resident_insu_license_no ?? null,
-            "partnerUniqueId" => "AC71",
+            "partnerUniqueId" => "EF222171",
             "role" => "Agent",
             "details" => "This is test agent",
             "state" => isset($user->internalAgentContract->state) ? $user->internalAgentContract->getState->name : null,
-            "managerPartnerUniqueId" => "EF222171",
+            "managerPartnerUniqueId" => "AC73",
 //            "uplineAgentEFNumber" => 'EF222171',
             "zipCode" => $user->internalAgentContract->zip ?? null,
         ];
