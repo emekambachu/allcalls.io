aecc0cdb6645555d5a6e1ac37f771e04d4e6a32f
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-02-13 16:22:19 +0000

    Add debug logs and validate and store CallDeviceAction

--- app/Http/Controllers/CallDeviceActionsController.php
+++ app/Http/Controllers/CallDeviceActionsController.php
@@ -3,26 +3,38 @@
 namespace App\Http\Controllers;
 
 use App\Models\Call;
+use App\Models\Device;
 use Illuminate\Http\Request;
 use App\Models\CallDeviceAction;
+use Illuminate\Support\Facades\Log;
 
 class CallDeviceActionsController extends Controller
 {
     public function store(Request $request)
     {
+        Log::debug('CallDeviceActionsController@store', $request->all());
+
         $validatedData = $request->validate([
             'call_id' => 'required|integer|exists:calls,id',
             'device_id' => 'required|integer|exists:devices,id',
             'action' => 'required|string|max:255',
         ]);
 
-        $call = Call::findOrFail($validatedData['call_id']);
+        Log::debug('CallDeviceActionsController@store:validated');
 
-        if ($request->user()->id !== $call->user_id) {
+        $call = Call::findOrFail($validatedData['call_id']);
+        $device = Device::findOrFail($validatedData['device_id']);
+
+        Log::debug('CallDeviceActionsController@store:call', $call->toArray());
+        Log::debug('CallDeviceActionsController@store:device', $device->toArray());
+
+        if ($request->user()->id !== $call->user_id && $request->user()->id !== $device->user_id) {
             return response()->json(['message' => 'Unauthorized'], 403);
         }
 
         $callDeviceAction = CallDeviceAction::create($validatedData);
+
+        Log::debug('CallDeviceActionsController@store:callDeviceStored', $callDeviceAction->toArray());
 
         return [
             'call_device_action' => $callDeviceAction
