8b6f468f0cf5e986db627fb0f5279a698d983a46
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-02-14 16:16:03 +0000

    Add openDetailedLogs function to show detailed logs for a call

--- resources/js/Pages/Admin/Calls/IndexBeta.vue
+++ resources/js/Pages/Admin/Calls/IndexBeta.vue
@@ -675,6 +675,12 @@ function formatDate(date) {
   return [year, month, day].join('-');
 }
 
+
+let showLogsForCall = ref(null);
+let openDetailedLogs = callId => {
+  showLogsForCall.value = callId;
+}
+
 </script>
 
 <template>
@@ -911,7 +917,6 @@ function formatDate(date) {
                     {{ userData.averageCallLength.toFixed(2) }} sec
                   </td>
                   <td class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap">
-                    <!-- Actions column content -->
                   </td>
                 </tr>
 
@@ -1166,7 +1171,10 @@ function formatDate(date) {
                     </div>
                   </td>
                   <td class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap">
-                    <!-- Actions column content -->
+                    <svg @click.prevent="openDetailedLogs(call.id)" title="Open Detailed Logs" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3 cursor-pointer text-gray-800 hover:text-gray-900">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
                   </td>
                 </tr>
               </tbody>
