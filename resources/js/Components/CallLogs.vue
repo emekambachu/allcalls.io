a0f535aec486493f728af53653c07d2e554b660f
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-02-14 21:56:38 +0000

    Add onMounted lifecycle hook to log the callId

--- resources/js/Components/CallLogs.vue
+++ resources/js/Components/CallLogs.vue
@@ -1,8 +1,14 @@
 <script setup>
+let { callId } = defineProps(['callId'])
+
+
+onMounted(() => {
+    console.log('Call ID:', callId);
+})
 </script>
 
 <template>
     <div class="bg-white p-2">
-        <h1>Call Logs</h1>
+        <h1>Call Logs for Call#{{ callId }}</h1>
     </div>
 </template>
