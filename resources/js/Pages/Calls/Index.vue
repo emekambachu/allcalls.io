6a0728c44b93e869925aced7be179942483549fd
awaisamir123 <awaisamir23@gmail.com>
2024-02-19 18:37:48 +0500

    disposition changes

--- resources/js/Pages/Calls/Index.vue
+++ resources/js/Pages/Calls/Index.vue
@@ -89,8 +89,8 @@ let addDisposition = (call , index) => {
   showDispositionModal.value = true
   call_id.value = call.id
   colIndex.value = index
-  if(call.disposition){
-    disposition.value = call.disposition
+  if(call?.get_client?.status){
+    disposition.value = call?.get_client?.status
   }else{
     disposition.value = 'Select a disposition'
   }
@@ -117,11 +117,17 @@ let saveChanges = () => {
     // router.visit('/admin/calls')
     disposition_loader.value = false
     showDispositionModal.value = false
-    props.calls.data[colIndex.value].disposition = res.data.call.disposition;
-    console.log('props.calls[colIndex.value]',props.calls[colIndex.value]);
+    if(props.calls.data[colIndex.value].get_client){
+      props.calls.data[colIndex.value].get_client.status = res.data.call?.client.status;
+    }
+    // console.log('props.calls[colIndex.value]',props.calls[colIndex.value]);
     // console.log('loadedCalls.value[colIndex.value]',loadedCalls.value[colIndex.value].disposition);
   }).catch((error)=>{
-    console.log('error',error);
+    if(error.response.status == 400){
+      toaster("error", error.response.data.errors);
+    }else{
+      toaster("error", error.message);
+    }
     disposition_loader.value = false
   })
 }
@@ -248,7 +254,7 @@ let saveChanges = () => {
                     DISPOSITION
                   </th>
                   <th scope="col" class="px-4 py-3" style="min-width: 175px">URL</th>
-                  <th scope="col" class="px-4 py-3 text-end" style="min-width: 110px">
+                  <th  scope="col" class="px-4 py-3 text-end" style="min-width: 150px;">
                     Actions
                   </th>
                 </tr>
@@ -276,7 +282,7 @@ let saveChanges = () => {
                   <td class="text-gray-600 px-4 py-3">{{ call.call_type.type }}</td>
                   <td class="text-gray-600 px-4 py-3">{{ call.from }}</td>
                   <td class="text-gray-600 px-4 py-3">
-                    {{ call.disposition }}
+                    {{ call?.get_client?.status }}
                     <!-- <span v-if="call.get_client">{{
                       call.get_client.status
                     }}</span>
@@ -312,9 +318,13 @@ let saveChanges = () => {
                       View Client
                     </button>
                     <button v-else class="text-center" type="button">-</button>
-                    <svg @click.prevent="addDisposition(call , index)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 cursor-pointer text-gray-800 hover:text-gray-900 ml-3">
+                    <button >
+
+                      <svg @click.prevent="addDisposition(call , index)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 cursor-pointer text-gray-800 hover:text-gray-900 ml-3">
                       <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                     </svg>
+                    </button>
+                   
                   </td>
                 </tr>
               </tbody>
