bbc4e61e0d9ccb4604890bcfe35aa257d6a3914a
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-02-20 21:31:23 +0000

    Update calendly event names in webhook controller

--- app/Http/Controllers/CalendlyWebhookController.php
+++ app/Http/Controllers/CalendlyWebhookController.php
@@ -19,13 +19,13 @@ class CalendlyWebhookController extends Controller
             if($request['payload']['status'] == 'active') {
                 $userExist = User::whereEmail($request['payload']['email'])->first();
                 if($userExist) {
-                    if($request['payload']['scheduled_event']['name'] == 'New Agent Call Review') {
+                    if($request['payload']['scheduled_event']['name'] == 'New Agent Training') {
                         $userExist->new_agent_call_scheduled = true;
                         $userExist->save();
                         Log::debug('New agent calendly meeting scheduled.');
                         return;
                     }
-                    elseif($request['payload']['scheduled_event']['name'] == 'New Agent Training'){
+                    elseif($request['payload']['scheduled_event']['name'] == 'New Agent Call Review'){
                         $userExist->low_balance_call_scheduled = true;
                         $userExist->save();
                         Log::debug('Low balance calendly meeting scheduled.');
@@ -37,13 +37,13 @@ class CalendlyWebhookController extends Controller
             if( $request['payload']['status'] == 'canceled') {
                 $userExist = User::whereEmail($request['payload']['email'])->first();
                 if($userExist) {
-                    if($request['payload']['scheduled_event']['name'] == 'New Agent Call Review') {
+                    if($request['payload']['scheduled_event']['name'] == 'New Agent Training') {
                         $userExist->new_agent_call_scheduled = false;
                         Log::debug('Low balance calendly meeting cancelled.');
                         $userExist->save();
                         return;
                     }
-                    elseif($request['payload']['scheduled_event']['name'] == 'New Agent Training'){
+                    elseif($request['payload']['scheduled_event']['name'] == 'New Agent Call Review'){
                         $userExist->low_balance_call_scheduled = false;
                         $userExist->save();
                         Log::debug('Low balance calendly meeting cancelled.');
