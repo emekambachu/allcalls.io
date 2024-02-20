5f66af29e196a2755c232be6c33311a9d5221995
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-02-20 18:12:39 +0000

    Add logging for low balance modal and open calendly link

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -25,7 +25,10 @@ if (page.props.auth.role !== "admin" && page.props.auth.user.balance < 40 && pag
 }
 
 let onLowBalanceModalClick = () => {
-  window.open("https://calendly.com/insurancecareers/new-agent-call-review", "_blank");
+  console.log('Opening low balance modal');
+  console.log(`https://calendly.com/insurancecareers/new-agent-call-review?name=${page.props.auth.user.first_name} ${page.props.auth.user.last_name}&email=${page.props.auth.user.email}`);
+
+  window.open(`https://calendly.com/insurancecareers/new-agent-call-review?name=${page.props.auth.user.first_name} ${page.props.auth.user.last_name}&email=${page.props.auth.user.email}`, "_blank");
   showLowBalanceModal.value = false;
 };
 
