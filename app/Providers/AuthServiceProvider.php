a896012d82eff64bfaefcbeac8972049f2610200
awaisamir123 <awaisamir23@gmail.com>
2024-02-20 21:45:00 +0500

    log viewer access

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -32,6 +32,7 @@ class AuthServiceProvider extends ServiceProvider
                 && in_array($request->user()->email, [
                     'ryan@pinnaclesyn.com',
                     'kat@allcalls.io',
+                    'awais@allcalls.io'
                 ]);
         });
 
