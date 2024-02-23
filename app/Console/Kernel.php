f889fbdcdbb7a57e76c43fe72f3e701341635818
awaisamir123 <awaisamir23@gmail.com>
2024-02-23 22:04:24 +0500

    update call tables columns sent_to_onscript

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,9 +28,7 @@ class Kernel extends ConsoleKernel
 
 
         $schedule->call(function () {
-            Log::debug('Daily call job:', [
-                'JOB RUNS' => "YES=>" .config('app.url'). ALLCALL_STAGING,
-            ]);
+
             // Check the condition before executing the command
             if(config('app.url')== ALLCALL_STAGING){
                 $calls = Call::whereSentToOnscript(false)->count();
