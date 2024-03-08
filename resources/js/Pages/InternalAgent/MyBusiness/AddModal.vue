d98951f8e7810508c24436282eeff997fea05b34
awaisamir123 <awaisamir23@gmail.com>
2024-03-08 06:37:17 -0600

    UTC preserve added in add modal mybusiness

--- resources/js/Pages/InternalAgent/MyBusiness/AddModal.vue
+++ resources/js/Pages/InternalAgent/MyBusiness/AddModal.vue
@@ -315,14 +315,12 @@ let Previous = (data) => {
   step.value -= 1;
 };
 let dateFormat = (val) => {
-  const date = new Date(val);
-  const toMonth = date.getMonth() + 1;
-  const toDate = date.getDate();
-  const toYear = date.getFullYear();
-
-  // Format the components as desired (e.g., as "MM-DD-YYYY")
-  // return `${toMonth}/${toDate}/${toYear}`;
-  return `${toYear}-${toMonth}-${toDate}`;
+    const date = new Date(val);
+    const dateString = date.toISOString().split('T')[0]; // Extracting the date part
+    const [year, month, day] = dateString.split('-'); // Splitting the date string
+// Create the formatted date string
+//     return `${month}-${day}-${year}`;
+    return `${year}-${month}-${day}`;
 };
 // save business data start
 let isLoading = ref(false);
@@ -1529,6 +1527,7 @@ let existingBusiness = () => {
                           format="MMM-dd-yyyy"
                           :maxDate="maxDate"
                           auto-apply
+                          utc="preserve"
                         >
                         </VueDatePicker>
                         <div
@@ -1654,7 +1653,7 @@ let existingBusiness = () => {
                       <div>
                         <label
                           class="block mt-5 text-sm mb-2 font-medium text-gray-900 "
-                          >Application Date<span class="text-red-400">*</span></label
+                          >Application Date <span class="text-red-400">*</span></label
                         >
                         <VueDatePicker
                           v-model="form.application_date"
@@ -1662,7 +1661,7 @@ let existingBusiness = () => {
                           format="MMM-dd-yyyy"
                           :maxDate="maxDate"
                           auto-apply
-                        >
+                          utc="preserve">
                         </VueDatePicker>
                         <div
                           v-if="firstStepErrors.application_date"
@@ -1890,6 +1889,7 @@ let existingBusiness = () => {
                           :disabled="disabledPolicydraftdate"
                           format="MMM-dd-yyyy"
                           auto-apply
+                          utc="preserve"
                         >
                         </VueDatePicker>
                         <div
