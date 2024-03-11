2f83eef12ed8cf6ca54a984b39b666268b064920
Victor Emeka Mbachu <victor.c.mbachu@gmail.com>
2024-03-11 18:59:23 +0000

    added report count to agent tab and fixed date filter format

---
+++ resources/js/Services/date-service.js
@@ -0,0 +1,31 @@
+let DateService = {
+
+    currentDateMDY(){
+        const today = new Date();
+        const yyyy = today.getFullYear();
+        let mm = today.getMonth() + 1; // Months start at 0!
+        let dd = today.getDate();
+
+        if (dd < 10) dd = '0' + dd;
+        if (mm < 10) mm = '0' + mm;
+
+        return mm + '/' + dd + '/' + yyyy;
+    },
+
+    formatDate(date){
+        let d = new Date(date),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
+
+        if (month.length < 2)
+            month = '0' + month;
+        if (day.length < 2)
+            day = '0' + day;
+
+        return [day, month ,year].join('-');
+    }
+
+}
+
+export default DateService;
