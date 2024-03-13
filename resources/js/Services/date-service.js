e335f632d9e21a9d0251c1da76de68f50283085e
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-03-13 08:27:17 -0400

    Refactor date formatting function for better clarity

--- resources/js/Services/date-service.js
+++ resources/js/Services/date-service.js
@@ -12,23 +12,41 @@ let DateService = {
         return mm + '/' + dd + '/' + yyyy;
     },
 
+    // formatDate(date, separator = '/') {
+    //     console.log('DateBefore', date);
+
+    //     let d = new Date(date),
+    //         month = '' + (d.getMonth() + 1),
+    //         day = '' + d.getDate(),
+    //         year = d.getFullYear();
+
+    //     if (month.length < 2)
+    //         month = '0' + month;
+    //     if (day.length < 2)
+    //         day = '0' + day;
+
+
+    //     console.log('DateAfter', [month, day, year].join(separator));
+
+    //     return [month, day, year].join(separator);
+    // }
+
+
     formatDate(date, separator = '/') {
-        console.log('DateBefore', date);
+        // Split the input date string into components
+        const [year, month, day] = dateStr.split('-').map(part => parseInt(part, 10));
 
-        let d = new Date(date),
-            month = '' + (d.getMonth() + 1),
-            day = '' + d.getDate(),
-            year = d.getFullYear();
+        // Create a new Date object using the local time zone
+        const d = new Date(year, month - 1, day);
 
-        if (month.length < 2)
-            month = '0' + month;
-        if (day.length < 2)
-            day = '0' + day;
+        // Format the date components, ensuring two digits
+        const formattedMonth = ('0' + (d.getMonth() + 1)).slice(-2);
+        const formattedDay = ('0' + d.getDate()).slice(-2);
+        const formattedYear = d.getFullYear();
 
+        // Join the components with the separator
+        return [formattedMonth, formattedDay, formattedYear].join(separator);
 
-        console.log('DateAfter', [month, day, year].join(separator));
-
-        return [month, day, year].join(separator);
     }
 
 }
