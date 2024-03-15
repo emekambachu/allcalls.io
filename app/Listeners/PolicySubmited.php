e8181acc4c2f998d7e9ae043c55a6aa977c19f4c
awaisamir123 <awaisamir23@gmail.com>
2024-03-16 00:47:37 +0500

    send message on new policy create using event

---
+++ app/Listeners/PolicySubmited.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\PolicySubmitedEvent;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Exception;
+
+class PolicySubmited
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(PolicySubmitedEvent $event): void
+    {
+        try {
+
+        $response = Http::withHeaders([
+            'Api-Token' => env('SENDBIRD_API_TOKEN'),
+            'Content-Type' => 'application/json',
+        ])->post('https://api-' . env('SENDBIRD_APPLICATION_ID') . '.sendbird.com/v3/group_channels/' . env('SENDBIRD_INTERNAL_AGENTS_GROUP_URL') . '/messages', [
+            'user_id' => env('SENDBIRD_ADMIN_ID'),
+            'message_type' => 'MESG',
+            'message' => "{$event->business->client->first_name} {$event->business->client->last_name} just made a sale! Awais Testing",
+        ]);
+            Log::debug("New Policy". $response->json());
+
+        } catch (Exception $e) {
+                 Log::error('PolicySubmitedEvent: ' . $e->getMessage());
+        }
+    }
+}
