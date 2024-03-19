4bda61f9f08b29a87f81ad34677be5ea40013d90
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-03-19 20:48:27 +0000

    Remove default value for 'state' field in clients table

--- database/migrations/2023_07_29_000044_create_clients_table.php
+++ database/migrations/2023_07_29_000044_create_clients_table.php
@@ -23,7 +23,6 @@ return new class extends Migration
             $table->string('dob')->nullable();
             $table->string('call_id');
             $table->unsignedInteger('user_id');
-            $table->string('state')->default('');
             $table->boolean('unlocked')->default(false);
             $table->string('beneficiary')->default('');
             $table->timestamps();
