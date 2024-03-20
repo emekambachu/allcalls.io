a185371fa404b708bfa5f59a8d7b65bbfabfc13a
awaisamir123 <awaisamir23@gmail.com>
2024-03-20 14:22:56 +0500

    PolicySubmitedEvent

--- app/Listeners/PolicySubmited.php
+++ app/Listeners/PolicySubmited.php
@@ -31,9 +31,8 @@ class PolicySubmited
             ])->post('https://api-' . env('SENDBIRD_APPLICATION_ID') . '.sendbird.com/v3/group_channels/' . env('SENDBIRD_INTERNAL_AGENTS_GROUP_URL') . '/messages', [
                 'user_id' => env('SENDBIRD_ADMIN_ID'),
                 'message_type' => 'MESG',
-                'message' => "{$event->business->client->first_name} {$event->business->client->last_name} just made a sale! Awais Testing",
+                'message' => "{$event->business->client->first_name} {$event->business->client->last_name} just made a sale!",
             ]);
-            dd($response->json());
             Log::debug("New Policy" . $response->json());
         } catch (Exception $e) {
             Log::error('PolicySubmitedEvent: ' . $e->getMessage());
