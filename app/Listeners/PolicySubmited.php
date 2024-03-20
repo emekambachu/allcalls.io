e3d48c2d54e936f460f98d8edb3fb30e74ef6ca6
awaisamir123 <awaisamir23@gmail.com>
2024-03-20 14:17:52 +0500

    PolicySubmitedEvent

--- app/Listeners/PolicySubmited.php
+++ app/Listeners/PolicySubmited.php
@@ -33,7 +33,7 @@ class PolicySubmited
                 'message_type' => 'MESG',
                 'message' => "{$event->business->client->first_name} {$event->business->client->last_name} just made a sale! Awais Testing",
             ]);
-            dd($response);
+            dd($response->json());
             Log::debug("New Policy" . $response->json());
         } catch (Exception $e) {
             Log::error('PolicySubmitedEvent: ' . $e->getMessage());
