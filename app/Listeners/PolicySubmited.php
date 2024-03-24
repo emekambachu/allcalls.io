4260e5b58e52fe2d3d29e691694a94fa73f48b91
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-03-24 01:21:54 +0000

    Update policy submission event handler to include agent details

--- app/Listeners/PolicySubmited.php
+++ app/Listeners/PolicySubmited.php
@@ -2,12 +2,13 @@
 
 namespace App\Listeners;
 
-use App\Events\PolicySubmitedEvent;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 use Exception;
+use App\Models\User;
+use App\Events\PolicySubmitedEvent;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
 class PolicySubmited
 {
@@ -25,14 +26,21 @@ class PolicySubmited
     public function handle(PolicySubmitedEvent $event): void
     {
         try {
+            $agentId = $event->business->agent_id;
+
+            $agent = User::find($agentId);
+
+            if (!$agent) {
+                throw new Exception("Agent not found with ID: $agentId");
+            }
+
             $response = Http::withHeaders([
                 'Api-Token' => env('SENDBIRD_API_TOKEN'),
                 'Content-Type' => 'application/json',
-            // ])->post('https://api-' . env('SENDBIRD_APPLICATION_ID') . '.sendbird.com/v3/group_channels/' . env('SENDBIRD_INTERNAL_AGENTS_GROUP_URL') . '/messages', [
             ])->post('https://api-' . env('SENDBIRD_APPLICATION_ID') . '.sendbird.com/v3/group_channels/' . env('SENDBIRD_INTERNAL_AGENTS_GROUP_URL_TEST') . '/messages', [
                 'user_id' => env('SENDBIRD_ADMIN_ID'),
                 'message_type' => 'MESG',
-                'message' => "{$event->business->client->first_name} {$event->business->client->last_name} just made a sale!",
+                'message' => "{$agent->first_name} {$agent->last_name} just made a sale!",
             ]);
 
             Log::debug("New Policy" . $response->body());
