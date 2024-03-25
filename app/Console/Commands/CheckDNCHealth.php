e8210b1b2ae7884679b8d4c12fc8ba58a470deff
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-03-25 18:19:36 +0000

    Update email body format to text/plain in CheckDNCHealth

--- app/Console/Commands/CheckDNCHealth.php
+++ app/Console/Commands/CheckDNCHealth.php
@@ -43,7 +43,7 @@ class CheckDNCHealth extends Command
             Mail::send([], [], function ($message) {
                 $message->to('iamfaizahmed123@gmail.com');
                 $message->subject('DNC Merge Successful');
-                $message->setBody('The DNC merge was successful.');
+                $message->setBody('The DNC merge was successful.', 'text/plain');
             });
         } else {
             $this->error('Sending failed email to the team...');
