3df5f9ca3f6e9ee6f64ac9ebec2fc92dd088818f
Muneeb Qadoos <iammmuneeb@gmail.com>
2024-03-31 16:52:12 +0100

    Update logging to include json encoding of call request

--- app/Http/Controllers/IncomingCallController.php
+++ app/Http/Controllers/IncomingCallController.php
@@ -28,7 +28,7 @@ class IncomingCallController extends Controller
 
         $to = $request->input('To');
         Log::debug('To attribute: ' . $to);
-        Log::info('Incoming call request from twilio: ' . $request->all());
+        Log::info('Incoming call request from twilio: ' . json_encode($request->all()));
 
         // Remove the "+1" from the beginning of the "To" number
         $to = substr($to, 2);
