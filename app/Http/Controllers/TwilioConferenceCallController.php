4289d23440d5848dcfe4d27c723ee5de0d3f9c87
Muneeb Qadoos <iammmuneeb@gmail.com>
2024-04-03 02:51:10 +0100

    Update callerId with storedCallerId variable

--- app/Http/Controllers/TwilioConferenceCallController.php
+++ app/Http/Controllers/TwilioConferenceCallController.php
@@ -181,7 +181,8 @@ class TwilioConferenceCallController extends Controller
                         "callToken" => $specialCallToken,
                         "StatusCallback" => route('conference.statusCallback'),
                         "StatusCallbackEvent" => ["initiated", "ringing", "answered", "completed"],
-                        "callerId" => $call->from
+                        // "callerId" => $call->from
+                        "callerId" => $storedCallerId
                     ]
                 );
 
