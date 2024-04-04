ac7ae819ccc628027abed324fd30cd1b9f5ccfd3
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-04-04 14:28:04 +0100

    Add click event to logout button in ResponsiveLogoutLink

--- resources/js/Components/ResponsiveLogoutLink.vue
+++ resources/js/Components/ResponsiveLogoutLink.vue
@@ -11,6 +11,7 @@ let logout = () => {
 
 <template>
   <button
+    @click.prevent="logout"
     class="block w-full pl-3 pr-4 py-2 border-l-4 border-transparent text-left text-base font-medium text-gray-300 hover:text-gray-400 hover:bg-cyan-950 hover:border-cyan-800 focus:outline-none focus:text-custom-white focus:bg-sky-950 focus:border-custom-blue transition duration-150 ease-in-out"
   >
     Logout
