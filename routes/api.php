fa436715c068639297757cd42afe0daaadecfde2
Muneeb Qadoos <iammmuneeb@gmail.com>
2024-04-18 22:09:01 +0100

    Remove commented out Twilio outbound call routes

--- routes/api.php
+++ routes/api.php
@@ -309,8 +309,8 @@ Route::post('/hangup-third-party', [TwilioConferenceCallController::class, 'hang
 Route::post('/hangup-self', [TwilioConferenceCallController::class, 'endCall'])->name('conference.hangupSelf');
 
 // Twilio outbound call routes
-Route::post('/call/outbound', [TwilioOutboundCallController::class, 'handleCall'])->name('outbound.makeCall');
-Route::post('/call/outbound/callback', [TwilioOutboundCallController::class, 'logTwilioRequest']);
+// Route::post('/call/outbound', [TwilioOutboundCallController::class, 'handleCall'])->name('outbound.makeCall');
+// Route::post('/call/outbound/callback', [TwilioOutboundCallController::class, 'logTwilioRequest']);
 
 Route::post('/twilio/sms/receive', [TwilioSMSController::class, 'receiveSMS']);
 Route::post('/commio/sms/send', [TextMessageController::class, 'sendMessage']);
